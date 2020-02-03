0b8142bf2ba75bba423dd9d704e3bdda5eace8b9
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-02-03 22:25:52 +0200

    bump formik

--- web/src/components/fields/switch.tsx
+++ web/src/components/fields/switch.tsx
@@ -18,14 +18,13 @@
 
 import React from 'react';
 import { Switch, SwitchProps } from 'pouncejs';
-import { useFormikContext, FieldConfig, useField } from 'formik';
+import { FieldConfig, useField } from 'formik';
 
 const FormikSwitch: React.FC<SwitchProps & Required<Pick<FieldConfig, 'name'>>> = props => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [field, meta] = useField<boolean>(props.name);
-  const { setFieldValue } = useFormikContext<any>();
+  const [field, meta, { setValue }] = useField<boolean>(props.name);
   return (
-    <Switch {...props} checked={field.value} onChange={value => setFieldValue(field.name, value)} />
+    <Switch {...props} checked={field.value} onChange={setValue} />
   );
 };
 
