400ec1239607adb847992961ce43907b6dc2851e
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-02-25 17:07:20 +0200

    add conditional husky config

--- husky.config.js
+++ husky.config.js
@@ -1,5 +1,11 @@
-{
-  "hooks": {
-    "pre-commit": "lint-staged"
-  }
+let huskyConfig = {};
+
+if (process.env.ENABLE_PANTHER_GIT_HOOKS) {
+  huskyConfig = {
+    hooks: {
+      'pre-commit': 'lint-staged',
+    },
+  };
 }
+
+module.exports = huskyConfig;
