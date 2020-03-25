f9a41db098219e9e9d45517d1cd573744e1c7ea7
Nick <49166439+nhakmiller@users.noreply.github.com>
2020-03-25 11:21:13 -0700

    Merge master into v1 docs (#569)
    * Updated remediation docs (#565)
    
    * updated remediation docs
    
    * addressed PR feedback
    
    * Parsers: Fluentd Syslog Support #437 (#535)
    
    * Parsers: Fluentd Syslog Support #437
    
    implement fluentd syslog parsers
    
    * add timestamp and tag field
    
    * add tests for fluentd timestamp parser and fix tests
    
    Co-authored-by: Nicholas Hakmiller <nicholas.hakmiller@runpanther.io>
    Co-authored-by: Russell Leighton <russell.leighton@runpanther.io>
    
    * Add fluentd parsers to UI and run fmt (#567)
    
    Co-authored-by: Sargon Sada <53259827+sardev08@users.noreply.github.com>
    Co-authored-by: Russell Leighton <russell.leighton@runpanther.io>

---
+++ internal/log_analysis/log_processor/parsers/fluentdsyslogs/rfc5424_test.go
@@ -0,0 +1,65 @@
+package fluentdsyslogs
+
+/**
+ * Panther is a scalable, powerful, cloud-native SIEM written in Golang/React.
+ * Copyright (C) 2020 Panther Labs Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import (
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/stretchr/testify/require"
+
+	"github.com/panther-labs/panther/internal/log_analysis/log_processor/parsers/testutil"
+	"github.com/panther-labs/panther/internal/log_analysis/log_processor/parsers/timestamp"
+)
+
+func TestRFC5424(t *testing.T) {
+	// nolint:lll
+	log := `{"pri": 16, "host": "192.168.0.1", "ident": "fluentd", "pid": "11111", "msgid": "ID24224", "extradata": "[exampleSDID@20224 iut=\"3\" eventSource=\"Application\" eventID=\"11211\"]","message": "[error] Syslog test", "tag":"syslog.authpriv.info","time":"2020-03-23 16:14:06 +0000"}`
+
+	expectedTime := time.Date(2020, 3, 23, 16, 14, 6, 0, time.UTC)
+	expectedRFC5424 := &RFC5424{
+		Priority:  aws.Uint8(16),
+		Hostname:  aws.String("192.168.0.1"),
+		Ident:     aws.String("fluentd"),
+		ProcID:    aws.String("11111"),
+		MsgID:     aws.String("ID24224"),
+		ExtraData: aws.String("[exampleSDID@20224 iut=\"3\" eventSource=\"Application\" eventID=\"11211\"]"),
+		Message:   aws.String("[error] Syslog test"),
+		Tag:       aws.String("syslog.authpriv.info"),
+		Timestamp: (*timestamp.FluentdTimestamp)(&expectedTime),
+	}
+
+	// panther fields
+	expectedRFC5424.PantherLogType = aws.String("Fluentd.Syslog5424")
+	expectedRFC5424.AppendAnyIPAddressPtrs(expectedRFC5424.Hostname)
+	expectedRFC5424.PantherEventTime = (*timestamp.RFC3339)(&expectedTime)
+	checkRFC5424(t, log, expectedRFC5424)
+}
+
+func TestRFC5424TypeType(t *testing.T) {
+	parser := &RFC5424Parser{}
+	require.Equal(t, "Fluentd.Syslog5424", parser.LogType())
+}
+
+func checkRFC5424(t *testing.T, log string, expectedRFC5424 *RFC5424) {
+	expectedRFC5424.SetEvent(expectedRFC5424)
+	parser := &RFC5424Parser{}
+	testutil.EqualPantherLog(t, expectedRFC5424.Log(), parser.Parse(log))
+}
