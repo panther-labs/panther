bea58c0716921e3abfae71762012d8209d88e233
Alex <alex.a.mylonas@gmail.com>
2020-04-03 18:53:08 +0300

    Added comment

--- web/src/helpers/utils.tsx
+++ web/src/helpers/utils.tsx
@@ -109,6 +109,7 @@ export const minutesToString = (minutes: number) =>
 export const convertObjArrayValuesToCsv = (obj: { [key: string]: any }) =>
   mapValues(obj, v => (Array.isArray(v) ? v.join(',') : v));
 
+/** URI encoding for specified fields from object */
 export const encodeParams = (obj: { [key: string]: any }, fields: [string]) => {
   const encodedObj = { ...obj };
   fields.forEach(field => {
