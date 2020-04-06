d7ce94a235f5e509ab5335b894f0aec2ef244295
Alex <alex.a.mylonas@gmail.com>
2020-04-06 22:31:38 +0300

    Changes in routes for Settings

--- web/src/urls.ts
+++ web/src/urls.ts
@@ -72,10 +72,11 @@ const urls = {
     },
   },
   settings: {
-    overview: () => `/settings/`,
-    general: () => `${urls.settings.overview()}general`,
-    users: () => `${urls.settings.overview()}users`,
-    destinations: () => `${urls.settings.overview()}destinations`,
+    home: () => '/settings/',
+    overview: () => `${urls.settings.home()}general/`,
+    general: () => `${urls.settings.home()}general/`,
+    users: () => `${urls.settings.home()}users/`,
+    destinations: () => `${urls.settings.home()}destinations/`,
   },
   account: {
     auth: {
