59a066b0731c7c77226a212f196270593016b354
Russell Leighton <russell.leighton@runpanther.io>
2020-04-13 17:04:48 -0400

    Checkpoint work

--- internal/log_analysis/datacatalog_updater/compaction/compaction_test.go
+++ internal/log_analysis/datacatalog_updater/compaction/compaction_test.go
@@ -1,5 +1,14 @@
 package compaction
 
+import (
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/glue"
+	"github.com/stretchr/testify/assert"
+)
+
 /**
  * Panther is a Cloud-Native SIEM for the Modern Security Team.
  * Copyright (C) 2020 Panther Labs Inc
@@ -18,20 +27,31 @@ package compaction
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/**
- * Panther is a scalable, powerful, cloud-native SIEM written in Golang/React.
- * Copyright (C) 2020 Panther Labs Inc
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+const (
+	testBucket = "testBucket"
+	testDb     = "panther_logs"
+	tableType  = "logs"
+	tableName  = "aws_cloudtrail"
+)
+
+func TestGenerateCtasSQL(t *testing.T) {
+	refTime := time.Date(2020, 4, 10, 5, 0, 0, 0, time.UTC)
+	expectedSQL := `
+create table panther_temp.panther_logs_logs_aws_cloudtrail_2020041005
+with (
+  external_location='s3://testBucket/logs/aws_cloudtrail/year=2020/month=04/day=10/hour=05/',
+  format='PARQUET', parquet_compression='UNCOMPRESSED'
+)
+as 
+select 
+c1,c2
+FROM "panther_logs"."aws_cloudtrail" where year=2020 and month=4 and day=10 and hour=5 order by p_event_time
+`
+	cols := []*glue.Column{
+		{Name: aws.String("c1")},
+		{Name: aws.String("c2")},
+	}
+	tableName, sql := GenerateCtasSQL(testDb, tableType, tableName, testBucket, cols, refTime)
+	assert.Equal(t, "panther_temp.panther_logs_logs_aws_cloudtrail_2020041005", tableName)
+	assert.Equal(t, expectedSQL, sql)
+}
--- internal/log_analysis/datacatalog_updater/compaction/compation.go
+++ internal/log_analysis/datacatalog_updater/compaction/compation.go
@@ -18,31 +18,20 @@ package compaction
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/**
- * Panther is a scalable, powerful, cloud-native SIEM written in Golang/React.
- * Copyright (C) 2020 Panther Labs Inc
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+import (
+	"fmt"
+	"strings"
+	"time"
 
-import "time"
+	"github.com/aws/aws-sdk-go/service/glue"
+)
 
 const (
+	ctasDatabase    = "panther_temp"
 	ctasSQLTemplate = `
-create table panther_temp.%s
+create table %s
 with (
-  external_location='s3://%s/%s/%s/year=%0d/month=%0d/day=%0d/hour=%0d/',
+  external_location='s3://%s/%s/%s/year=%d/month=%02d/day=%02d/hour=%02d/',
   format='PARQUET', parquet_compression='UNCOMPRESSED'
 )
 as 
@@ -52,6 +41,22 @@ FROM "%s"."%s" where year=%d and month=%d and day=%d and hour=%d order by p_even
 `
 )
 
-func GenerateCtasSQL(databaseName, tableName, bucketName string, hour time.Time) (ctas string, err error) {
-	return ctas, err
+func GenerateCtasSQL(databaseName, tableType, tableName, bucketName string, columns []*glue.Column,
+	hour time.Time) (tempTable, ctsa string) {
+
+	// generate name for table, by using this key it will fail if another is tried concurrently
+	tempTable = ctasDatabase + "." + databaseName + "_" + tableType + "_" + tableName + "_" + hour.Format("2006010215")
+
+	// list the columns
+	selectCols := make([]string, len(columns))
+	for i := range columns {
+		selectCols[i] = *columns[i].Name
+	}
+
+	return tempTable, fmt.Sprintf(ctasSQLTemplate,
+		tempTable,
+		bucketName, tableType, tableName, hour.Year(), hour.Month(), hour.Day(), hour.Hour(),
+		strings.Join(selectCols, ","),
+		databaseName, tableName, hour.Year(), hour.Month(), hour.Day(), hour.Hour(),
+	)
 }
