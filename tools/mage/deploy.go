b62a7daccae9d5d9b6f4da6dce0fe0606a4f662f
Russell Leighton <russell.leighton@runpanther.io>
2020-04-18 08:08:14 -0400

    Add usage examples in comments for PrincipalARNs and S3ARNs in panther_config.yml

--- tools/mage/deploy.go
+++ tools/mage/deploy.go
@@ -192,17 +192,8 @@ func bootstrap(awsSession *session.Session, settings *config.PantherConfig) map[
 
 	// Deploy first bootstrap stack
 	go func() {
-		// the example yml has an empty string to make it clear it is a list, remove empty strings
-		var sanitizedLogSubscriptionArns []string
-		for _, arn := range settings.Setup.LogSubscriptions.PrincipalARNs {
-			if arn == "" {
-				continue
-			}
-			sanitizedLogSubscriptionArns = append(sanitizedLogSubscriptionArns, arn)
-		}
-
 		params := map[string]string{
-			"LogSubscriptionPrincipals":  strings.Join(sanitizedLogSubscriptionArns, ","),
+			"LogSubscriptionPrincipals":  strings.Join(settings.Setup.LogSubscriptions.PrincipalARNs, ","),
 			"EnableS3AccessLogs":         strconv.FormatBool(settings.Setup.EnableS3AccessLogs),
 			"AccessLogsBucket":           settings.Setup.S3AccessLogsBucket,
 			"CertificateArn":             certificateArn(awsSession, settings),
@@ -369,16 +360,8 @@ func deployMainStacks(awsSession *session.Session, settings *config.PantherConfi
 	// Core
 	parallelStacks++
 	go func(result chan string) {
-		// the example yml has an empty string to make it clear it is a list, remove empty strings
-		var sanitizedAthenaS3Arns []string
-		for _, arn := range settings.Setup.Athena.S3ARNs {
-			if arn == "" {
-				continue
-			}
-			sanitizedAthenaS3Arns = append(sanitizedAthenaS3Arns, arn)
-		}
-		// add in the panther buckets
-		sanitizedAthenaS3Arns = append(sanitizedAthenaS3Arns, "arn:aws:s3:::panther-*-processeddata-*")
+		// add in the internal panther buckets
+		settings.Setup.Athena.S3ARNs = append(settings.Setup.Athena.S3ARNs, "arn:aws:s3:::panther-*-processeddata-*")
 
 		deployTemplate(awsSession, coreTemplate, sourceBucket, coreStack, map[string]string{
 			"AppDomainURL":           outputs["LoadBalancerUrl"],
@@ -392,7 +375,7 @@ func deployMainStacks(awsSession *session.Session, settings *config.PantherConfi
 			"SqsKeyId":               outputs["QueueEncryptionKeyId"],
 			"UserPoolId":             outputs["UserPoolId"],
 
-			"AthenaS3BucketARNS":         strings.Join(sanitizedAthenaS3Arns, ","),
+			"AthenaS3BucketARNS":         strings.Join(settings.Setup.Athena.S3ARNs, ","),
 			"CloudWatchLogRetentionDays": strconv.Itoa(settings.Monitoring.CloudWatchLogRetentionDays),
 			"Debug":                      strconv.FormatBool(settings.Monitoring.Debug),
 			"LayerVersionArns":           settings.Infra.BaseLayerVersionArns,
