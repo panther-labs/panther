9e2014dc41212ec72dc6d7cbdacd0c6dd14ea2b4
Russell Leighton <russell.leighton@runpanther.io>
2020-04-18 09:35:29 -0400

    Revert change to log level in genericapi invoke

--- pkg/genericapi/invoke.go
+++ pkg/genericapi/invoke.go
@@ -56,7 +56,7 @@ func Invoke(
 		return &InternalError{Message: "jsoniter.Marshal(input) failed: " + err.Error()}
 	}
 
-	zap.L().Debug(
+	zap.L().Info(
 		"invoking Lambda function", zap.String("name", function), zap.Int("bytes", len(payload)))
 	response, err := client.Invoke(
 		&lambda.InvokeInput{FunctionName: aws.String(function), Payload: payload})
