a89c0925dd096eaca38c611b1f619d929c99dcac
Alex <alex.a.mylonas@gmail.com>
2020-05-04 15:58:16 +0300

    Fixed breadcrumb module url

--- web/src/urls.ts
+++ web/src/urls.ts
@@ -74,7 +74,7 @@ const urls = {
   settings: {
     home: () => '/settings/',
     general: () => `${urls.settings.home()}general/`,
-    globalModule: () => `${urls.settings.home()}/module/`,
+    globalModule: () => `${urls.settings.home()}module/`,
     users: () => `${urls.settings.home()}users/`,
     destinations: () => `${urls.settings.home()}destinations/`,
   },
