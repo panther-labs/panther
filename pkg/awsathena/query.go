96adeaeb7f02e5c6b5096553353a6d36636daea9
Russell Leighton <russell.leighton@runpanther.io>
2020-05-06 09:54:39 -0400

    Respnd to PR

--- pkg/awsathena/query.go
+++ pkg/awsathena/query.go
@@ -66,9 +66,12 @@ func WaitForResults(client athenaiface.AthenaAPI, queryExecutionID string) (quer
 		switch *executionOutput.QueryExecution.Status.State {
 		case
 			athena.QueryExecutionStateSucceeded,
-			athena.QueryExecutionStateFailed,
 			athena.QueryExecutionStateCancelled:
 			return executionOutput, true, nil
+		case
+			athena.QueryExecutionStateFailed:
+			return executionOutput, true,
+				errors.Errorf("query execution failed: %s", *executionOutput.QueryExecution.Status.StateChangeReason)
 		default:
 			return executionOutput, false, nil
 		}
@@ -81,10 +84,6 @@ func WaitForResults(client athenaiface.AthenaAPI, queryExecutionID string) (quer
 				return nil, err
 			}
 			if done {
-				if *executionOutput.QueryExecution.Status.State == athena.QueryExecutionStateFailed {
-					err = errors.Errorf("query execution failed: %s",
-						*executionOutput.QueryExecution.Status.StateChangeReason)
-				}
 				return executionOutput, err
 			}
 			time.Sleep(pollDelay)
