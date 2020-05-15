6922f141d512ea042240a636ec80caa75427dbd8
Kostas Papageorgiou <kostas@runpanther.io>
2020-05-15 13:07:44 +0200

    Removing AWS dependency from CSV utils

--- internal/log_analysis/log_processor/parsers/csv_utils.go
+++ internal/log_analysis/log_processor/parsers/csv_utils.go
@@ -1,35 +1,15 @@
 package parsers
 
-/**
- * Panther is a Cloud-Native SIEM for the Modern Security Team.
- * Copyright (C) 2020 Panther Labs Inc
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 import (
 	"strconv"
 	"strings"
-
-	"github.com/aws/aws-sdk-go/aws"
 )
 
 func CsvStringToPointer(value string) *string {
 	if value == "-" {
 		return nil
 	}
-	return aws.String(value)
+	return &value
 }
 
 func CsvStringToIntPointer(value string) *int {
@@ -40,18 +20,19 @@ func CsvStringToIntPointer(value string) *int {
 	if err != nil {
 		return nil
 	}
-	return aws.Int(result)
+	return &result
 }
 
 func CsvStringToInt16Pointer(value string) *int16 {
 	if value == "-" {
 		return nil
 	}
-	result, err := strconv.Atoi(value)
+	result, err := strconv.ParseInt(value, 10, 16)
 	if err != nil {
 		return nil
 	}
-	return aws.Int16(int16(result))
+	returnValue := int16(result)
+	return &returnValue
 }
 
 func CsvStringToFloat64Pointer(value string) *float64 {
@@ -62,7 +43,7 @@ func CsvStringToFloat64Pointer(value string) *float64 {
 	if err != nil {
 		return nil
 	}
-	return aws.Float64(result)
+	return &result
 }
 
 func CsvStringToArray(value string) []string {
