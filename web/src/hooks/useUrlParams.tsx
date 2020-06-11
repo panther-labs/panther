0a5c70cfaa6d0b5210670ece047cd2afb22e295a
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-06-11 14:20:49 +0300

    refactor history redirects

--- web/src/hooks/useUrlParams.tsx
+++ web/src/hooks/useUrlParams.tsx
@@ -1,19 +1,10 @@
 /**
- * Panther is a Cloud-Native SIEM for the Modern Security Team.
  * Copyright (C) 2020 Panther Labs Inc
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Panther Enterprise is licensed under the terms of a commercial license available from
+ * Panther Labs Inc ("Panther Commercial License") by contacting contact@runpanther.com.
+ * All use, distribution, and/or modification of this software, whether commercial or non-commercial,
+ * falls under the Panther Commercial License to the extent it is permitted.
  */
 
 import React from 'react';
@@ -28,12 +19,12 @@ const queryStringOptions = {
 };
 
 function useUrlParams<T extends { [key: string]: any }>() {
-  const { history } = useRouter();
+  const { history, location } = useRouter();
 
   /**
    * parses the query params of a URL and returns an object with params in the correct typo
    */
-  const urlParams = queryString.parse(history.location.search, queryStringOptions) as T;
+  const urlParams = queryString.parse(location.search, queryStringOptions) as T;
 
   /**
    * stringifies an object and adds it to the existing query params of a URL
@@ -50,12 +41,10 @@ function useUrlParams<T extends { [key: string]: any }>() {
       v => !v && !['number', 'boolean'].includes(typeof v)
     );
 
-    history.replace(
-      `${history.location.pathname}?${queryString.stringify(
-        cleanedMergedQueryParams,
-        queryStringOptions
-      )}`
-    );
+    history.replace({
+      ...location,
+      search: queryString.stringify(cleanedMergedQueryParams, queryStringOptions),
+    });
   };
 
   // Cache those values as long as URL parameters are the same
