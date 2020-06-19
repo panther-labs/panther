dc5d1c3fad31c4f726d4bd7e12637786d9daa433
Nicholas Hakmiller <nicholas.hakmiller@runpanther.io>
2020-06-19 16:24:37 -0700

    read  only custom resource

--- internal/core/custom_resources/resources/layer_attachment.go
+++ internal/core/custom_resources/resources/layer_attachment.go
@@ -29,6 +29,8 @@ import (
 	"go.uber.org/zap"
 )
 
+const latest = "LATEST"
+
 type LayerAttachmentProperties struct {
 	FunctionArn *string
 	LayerArns   []*string
@@ -39,7 +41,7 @@ func customLayerAttachment(_ context.Context, event cfn.Event) (string, map[stri
 	case cfn.RequestCreate, cfn.RequestUpdate:
 		return handleCreateUpdateRequests(event)
 	case cfn.RequestDelete:
-		return handleDeleteRequests(event)
+		return event.PhysicalResourceID, nil, nil
 	default:
 		return "", nil, fmt.Errorf("unknown request type %s", event.RequestType)
 	}
@@ -48,176 +50,54 @@ func customLayerAttachment(_ context.Context, event cfn.Event) (string, map[stri
 func handleCreateUpdateRequests(event cfn.Event) (string, map[string]interface{}, error) {
 	var newProps LayerAttachmentProperties
 	// Parse the properties
-	if err := parseProperties(event.ResourceProperties, &newProps); err != nil {
-		return "", nil, err
-	}
-	// Separate the version so we can do string comparisons
-	newLayers, err := extractToLayers(newProps.LayerArns)
+	err := parseProperties(event.ResourceProperties, &newProps)
 	if err != nil {
 		return "", nil, err
-	}
-
-	// Repeat for the old state
-	var oldProps LayerAttachmentProperties
-	if err := parseProperties(event.OldResourceProperties, &oldProps); err != nil {
-		return "", nil, err
-	}
-	oldLayers, err := extractToLayers(oldProps.LayerArns)
-	if err != nil {
-		return "", nil, err
-	}
-
-	// Lambda does not allow partial updates to the function layer configuration, so we need to
-	// look up any existing layers
-	response, err := lambdaClient.GetFunctionConfiguration(&lambda.GetFunctionConfigurationInput{
-		FunctionName: newProps.FunctionArn,
-	})
-	if err != nil {
-		return "", nil, err
-	}
-
-	// Insert/update/remove layers as appropriate
-
-	// First determine if there are any existing layers that need to be removed
-	var layersToRemove []*Layer
-	for _, layer := range oldLayers {
-		found := false
-		for _, newLayer := range newLayers {
-			if layer.Arn == newLayer.Arn {
-				found = true
-				break
-			}
-		}
-		if !found {
-			layersToRemove = append(layersToRemove, layer)
-		}
 	}
 
 	var finalLayers []*string
-	// Add all the new layers because we know we want them
-	for _, layer := range newLayers {
-		versionedArn := layer.Arn + ":" + strconv.Itoa(layer.Version)
+	for _, layer := range newProps.LayerArns {
+		pieces := strings.Split(*layer, ":")
+		// Check if the version (the last section of the arn) is equal to latest
+		version := pieces[len(pieces)-1]
+		arn := strings.Join(pieces[0:len(pieces)-1], ":")
+		if version == latest {
+			version, err = getLatestLayerVersion(arn)
+			if err != nil {
+				return "", nil, err
+			}
+		}
+		// This means the layer doesn't exist yet, the service that creates the layer will be
+		// responsible for attaching it or the deploy will need to be run again
+		if version == "" {
+			continue
+		}
+		versionedArn := arn + ":" + version
 		finalLayers = append(finalLayers, &versionedArn)
 	}
 
-	// Add every existing layer that we don't need to remove
-	for _, existingLayer := range response.Layers {
-		// Filter out what we know needs to be removed
-		skip := false
-		for _, removedLayer := range layersToRemove {
-			if strings.HasPrefix(*existingLayer.Arn, removedLayer.Arn) {
-				skip = true
-				break
-			}
-		}
-		if skip {
-			continue
-		}
-
-		// Determine which layers may need to be maintained
-		keep := true
-		for _, newLayer := range newLayers {
-			if strings.HasPrefix(*existingLayer.Arn, newLayer.Arn) {
-				keep = false
-				break
-			}
-		}
-		if keep {
-			finalLayers = append(finalLayers, existingLayer.Arn)
-		}
-	}
-
 	zap.L().Info("adding layers", zap.Any("finalLayers", finalLayers))
-	_, err = lambdaClient.UpdateFunctionConfiguration(&lambda.UpdateFunctionConfigurationInput{
-		FunctionName: newProps.FunctionArn,
-		Layers:       finalLayers,
-	})
-	if err != nil {
-		return "", nil, err
-	}
-
 	resourceID := fmt.Sprintf("custom:lambda:layerattachment:%s",
 		*newProps.FunctionArn)
 
-	return resourceID, nil, nil
+	return resourceID, map[string]interface{}{"LayerArns": finalLayers}, nil
 }
 
-func handleDeleteRequests(event cfn.Event) (string, map[string]interface{}, error) {
-	split := strings.Split(event.PhysicalResourceID, ":")
-	if len(split) < 4 {
-		// invalid resourceID (e.g. CREATE_FAILED) - skip delete
-		return event.PhysicalResourceID, nil, nil
-	}
-	var props LayerAttachmentProperties
-	if err := parseProperties(event.ResourceProperties, &props); err != nil {
-		return "", nil, err
-	}
-	// Separate the version so we can do string comparisons
-	layersToRemove, err := extractToLayers(props.LayerArns)
-	if err != nil {
-		return "", nil, err
-	}
-
-	// Lambda does not allow partial updates to the function layer configuration, so we need to
-	// look up any existing layers
-	response, err := lambdaClient.GetFunctionConfiguration(&lambda.GetFunctionConfigurationInput{
-		FunctionName: props.FunctionArn,
+func getLatestLayerVersion(layerArn string) (string, error) {
+	response, err := lambdaClient.ListLayerVersions(&lambda.ListLayerVersionsInput{
+		LayerName: &layerArn,
 	})
-	// Function is probably already deleted, so just exit
 	if err != nil {
-		return event.PhysicalResourceID, nil, err
+		return "", err
 	}
-
-	var finalLayers []*string
-	for _, existingLayer := range response.Layers {
-		// Filter out what we know needs to be removed
-		keep := true
-		for _, layer := range layersToRemove {
-			if strings.HasPrefix(*existingLayer.Arn, layer.Arn) {
-				keep = false
-				break
-			}
-		}
-		if keep {
-			finalLayers = append(finalLayers, existingLayer.Arn)
+	greatest := int64(-1)
+	for _, layer := range response.LayerVersions {
+		if *layer.Version > greatest {
+			greatest = *layer.Version
 		}
 	}
-
-	_, err = lambdaClient.UpdateFunctionConfiguration(&lambda.UpdateFunctionConfigurationInput{
-		FunctionName: props.FunctionArn,
-		Layers:       finalLayers,
-	})
-	return event.PhysicalResourceID, nil, err
-}
-
-type Layer struct {
-	Arn     string
-	Version int
-}
-
-func extractToLayers(layerArns []*string) ([]*Layer, error) {
-	var layers []*Layer
-	for _, layerArn := range layerArns {
-		layer, err := extractLayerVersion(layerArn)
-		if err != nil {
-			return nil, err
-		}
-		layers = append(layers, layer)
+	if greatest == -1 {
+		return "", nil
 	}
-
-	return layers, nil
-}
-
-func extractLayerVersion(layerArn *string) (*Layer, error) {
-	pieces := strings.Split(*layerArn, ":")
-	version, err := strconv.Atoi(pieces[len(pieces)-1])
-	if err != nil {
-		return nil, err
-	}
-	arn := strings.Join(pieces[0:len(pieces)-1], ":")
-
-	return &Layer{
-		Arn:     arn,
-		Version: version,
-	}, nil
+	return strconv.Itoa(int(greatest)), nil
 }
