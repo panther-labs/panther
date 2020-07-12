2601ea6088fe831b54e41c18edf26209069ec955
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-07-12 12:27:34 +0300

    refactor: file name

--- web/__tests__/jest.config.js
+++ web/__tests__/jest.config.js
@@ -26,7 +26,7 @@ module.exports = {
 
   // This is the only way for jest to detect our custom webpack aliases
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|svg)$': '<rootDir>/__mocks__/fileMock.ts',
+    '\\.(jpg|jpeg|png|svg)$': '<rootDir>/__mocks__/file.ts',
     '^lodash-es/(.*)': 'lodash/$1',
     '^Assets/(.*)': '<rootDir>/../src/assets/$1',
     '^Components/(.*)': '<rootDir>/../src/components/$1',
