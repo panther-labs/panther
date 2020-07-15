b6b1a83b0c8663971d5e30d41bdd7232b90a0d91
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-07-15 12:44:46 +0300

    test: checking config in CI

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -116,7 +116,7 @@ module.exports = {
     },
   },
   performance: {
-    hints: process.env.CI ? false : 'warning',
+    hints: false,
   },
   module: {
     // enforce a javascript `strict` mode on different files
