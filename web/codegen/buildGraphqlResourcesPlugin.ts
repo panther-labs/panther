a966681d93782ae5b2afcc1ad6b1b9f0e3dfe30a
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-07-17 15:37:35 +0300

    update seed

--- web/codegen/buildGraphqlResourcesPlugin.ts
+++ web/codegen/buildGraphqlResourcesPlugin.ts
@@ -37,7 +37,7 @@ import faker from 'faker';
 // monkeypatching the date func since seeding is broken without it
 // https://github.com/Marak/faker.js/issues/608#issuecomment-491345249
 const oldPast = faker.date.past;
-faker.date.past = (years = 1, refDate = new Date(2021, 0, 0)) =>
+faker.date.past = (years = 1, refDate = new Date(2020, 0, 0)) =>
   oldPast.call(faker.date, years, refDate);
 
 const toMockName = (str: string) => `build${str}`;
