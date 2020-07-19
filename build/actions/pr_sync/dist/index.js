e9dfe2153dabe0ebaffeb9e975a3289288993be7
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-07-19 23:12:11 +0300

    feat: add author as reviewer

--- build/actions/pr_sync/dist/index.js
+++ build/actions/pr_sync/dist/index.js
@@ -743,7 +743,8 @@ module.exports = /******/ (function (modules, runtime) {
           const token = core.getInput('token');
 
           // Get the JSON webhook payload for the event that triggered the workflow
-          const { pull_request: pullRequest } = github.context.payload;
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          const pullRequest = github.context.payload.pull_request;
 
           // If PR was closed, but it was not due to it being merged, then do nothing
           if (!pullRequest.merged) {
@@ -752,8 +753,8 @@ module.exports = /******/ (function (modules, runtime) {
           core.debug('PR was closed due to a merge. Looking for ignore labels...');
 
           // If PR has the "ignore" label, then the PR sync should not happen
-          const isBackport = pullRequest.labels.some(label => label.name === ignoreLabel);
-          if (isBackport) {
+          const shouldIgnore = pullRequest.labels.some(label => label.name === ignoreLabel);
+          if (shouldIgnore) {
             return;
           }
           core.debug('PR did not have an ignore label. Starting sync process...');
@@ -763,12 +764,14 @@ module.exports = /******/ (function (modules, runtime) {
           core.debug('Octokit instance setup successfully');
 
           // https://developer.github.com/v3/git/refs/#create-a-reference
+          core.debug('Creating PR branch...');
           const prBranchName = pullRequest.head.ref;
           await octokit.request(`POST /repos/${destRepo}/git/refs`, {
             ref: `refs/heads/${prBranchName}`,
             sha: pullRequest.merge_commit_sha,
           });
 
+          // https://developer.github.com/v3/pulls/#create-a-pull-request
           core.debug('Creating a pull request...');
           const destPullRequest = await octokit.request(`POST /repos/${destRepo}/pulls`, {
             title: `${PR_TITLE_PREFIX} ${pullRequest.title}`,
@@ -779,21 +782,22 @@ module.exports = /******/ (function (modules, runtime) {
             draft: false,
           });
 
-          // Clone the existing labels
-          core.debug(destPullRequest);
-
           // https://developer.github.com/v3/issues/#update-an-issue
           core.debug('Setting assignees, labels & milestone...');
+          await octokit.request(`POST /repos/${destRepo}/issues/${destPullRequest.id}`, {
+            assignees: pullRequest.assignees.map(assignee => assignee.login),
+            labels: pullRequest.labels.map(label => label.name),
+            milestone: pullRequest.milestone ? pullRequest.milestone.id : null,
+          });
+
+          // https://developer.github.com/v3/pulls/review_requests/#request-reviewers-for-a-pull-request
+          core.debug('Setting reviewers...');
           await octokit.request(
-            `POST /repos/${destRepo}/pulls/repos/${destRepo}/issues/${destPullRequest.id}`,
+            `POST /repos/${destRepo}/pulls/${destPullRequest.id}/requested_reviewers`,
             {
-              assignees: pullRequest.assignees.map(assignee => assignee.login),
-              labels: pullRequest.labels.map(label => label.name),
-              milestone: pullRequest.milestone.id,
+              reviewers: pullRequest.user.login,
             }
           );
-
-          // TODO: add reviewer
 
           // Set the `url` output to the created PR's URL
           core.setOutput('url', destPullRequest.url);
--- build/actions/pr_sync/index.js
+++ build/actions/pr_sync/index.js
@@ -12,7 +12,8 @@ const main = async () => {
     const token = core.getInput('token');
 
     // Get the JSON webhook payload for the event that triggered the workflow
-    const { pull_request: pullRequest } = github.context.payload;
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const pullRequest = github.context.payload.pull_request;
 
     // If PR was closed, but it was not due to it being merged, then do nothing
     if (!pullRequest.merged) {
@@ -21,8 +22,8 @@ const main = async () => {
     core.debug('PR was closed due to a merge. Looking for ignore labels...');
 
     // If PR has the "ignore" label, then the PR sync should not happen
-    const isBackport = pullRequest.labels.some(label => label.name === ignoreLabel);
-    if (isBackport) {
+    const shouldIgnore = pullRequest.labels.some(label => label.name === ignoreLabel);
+    if (shouldIgnore) {
       return;
     }
     core.debug('PR did not have an ignore label. Starting sync process...');
@@ -32,12 +33,14 @@ const main = async () => {
     core.debug('Octokit instance setup successfully');
 
     // https://developer.github.com/v3/git/refs/#create-a-reference
+    core.debug('Creating PR branch...');
     const prBranchName = pullRequest.head.ref;
     await octokit.request(`POST /repos/${destRepo}/git/refs`, {
       ref: `refs/heads/${prBranchName}`,
       sha: pullRequest.merge_commit_sha,
     });
 
+    // https://developer.github.com/v3/pulls/#create-a-pull-request
     core.debug('Creating a pull request...');
     const destPullRequest = await octokit.request(`POST /repos/${destRepo}/pulls`, {
       title: `${PR_TITLE_PREFIX} ${pullRequest.title}`,
@@ -48,21 +51,22 @@ const main = async () => {
       draft: false,
     });
 
-    // Clone the existing labels
-    core.debug(destPullRequest);
-
     // https://developer.github.com/v3/issues/#update-an-issue
     core.debug('Setting assignees, labels & milestone...');
+    await octokit.request(`POST /repos/${destRepo}/issues/${destPullRequest.id}`, {
+      assignees: pullRequest.assignees.map(assignee => assignee.login),
+      labels: pullRequest.labels.map(label => label.name),
+      milestone: pullRequest.milestone ? pullRequest.milestone.id : null,
+    });
+
+    // https://developer.github.com/v3/pulls/review_requests/#request-reviewers-for-a-pull-request
+    core.debug('Setting reviewers...');
     await octokit.request(
-      `POST /repos/${destRepo}/pulls/repos/${destRepo}/issues/${destPullRequest.id}`,
+      `POST /repos/${destRepo}/pulls/${destPullRequest.id}/requested_reviewers`,
       {
-        assignees: pullRequest.assignees.map(assignee => assignee.login),
-        labels: pullRequest.labels.map(label => label.name),
-        milestone: pullRequest.milestone.id,
+        reviewers: pullRequest.user.login,
       }
     );
-
-    // TODO: add reviewer
 
     // Set the `url` output to the created PR's URL
     core.setOutput('url', destPullRequest.url);
