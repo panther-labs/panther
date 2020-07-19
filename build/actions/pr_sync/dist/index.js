984c571cdb1e83ad1dd1c4994dc9c3a49e235b16
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-07-19 20:06:34 +0300

    build: build action

--- build/actions/pr_sync/dist/index.js
+++ build/actions/pr_sync/dist/index.js
@@ -732,17 +732,75 @@ module.exports = /******/ (function (modules, runtime) {
       const core = __webpack_require__(470);
       const github = __webpack_require__(469);
 
-      try {
-        const destRepo = core.getInput('destRepo');
+      const PR_TITLE_PREFIX = '[OSS Sync]';
+      const MASTER_BRANCH = 'master';
 
-        core.setOutput('url', `https://github.com/${destRepo}`);
-        // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2);
-        // eslint-disable-next-line no-console
-        console.log(`The event payload: ${payload}`);
-      } catch (error) {
-        core.setFailed(error.message);
-      }
+      const main = async () => {
+        try {
+          core.debug('Initializing...');
+          const destRepo = core.getInput('destRepo');
+          const ignoreLabel = core.getInput('ignoreLabel');
+          const token = core.getInput('token');
+
+          // Get the JSON webhook payload for the event that triggered the workflow
+          const { pull_request: pullRequest } = github.context.payload;
+
+          // If PR was closed, but it was not due to it being merged, then do nothing
+          if (!pullRequest.merged) {
+            return;
+          }
+          core.debug('PR was closed due to a merge. Looking for ignore labels...');
+
+          // If PR has the "ignore" label, then the PR sync should not happen
+          const isBackport = pullRequest.labels.some(label => label.name === ignoreLabel);
+          if (isBackport) {
+            return;
+          }
+          core.debug('PR did not have an ignore label. Starting sync process...');
+
+          // const octokit = new Octokit({ auth: token });
+          core.debug('Initializing octokit...');
+          const octokit = github.getOctokit(token);
+          core.debug('Octokit instance setup successfully');
+
+          core.debug('Creating a pull request...');
+          const destPullRequest = await octokit(`POST /repos/${destRepo}/pulls`, {
+            title: `${PR_TITLE_PREFIX} ${pullRequest.title}`,
+            body: pullRequest.body,
+            maintainer_can_modify: true,
+            head: pullRequest.head.label,
+            base: MASTER_BRANCH,
+            draft: false,
+          });
+
+          // Clone the existing labels
+          core.debug(destPullRequest);
+          await octokit(
+            `POST /repos/${destRepo}/pulls``/repos/${destRepo}/issues/${destPullRequest.id}/labels`,
+            {
+              labels: pullRequest.labels.map(label => label.name),
+            }
+          );
+
+          // Clone the existing labels
+          await octokit(
+            `POST /repos/${destRepo}/pulls``/repos/${destRepo}/issues/${destPullRequest.id}`,
+            {
+              labels: pullRequest.labels.map(label => label.name),
+              milestone: pullRequest.milestone.id,
+            }
+          );
+
+          // Set the `url` output to the created PR's URL
+          core.setOutput('url', destPullRequest.url);
+        } catch (error) {
+          core.setFailed(error.message);
+        } finally {
+          // noop
+        }
+      };
+
+      main();
 
       /***/
     },
