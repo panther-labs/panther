5108fcacaf656bc73072e393ff00203501d5e407
Ben Pruce <bpruce@gmail.com>
2020-07-21 11:45:45 -0400

    update constants

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -133,7 +133,7 @@ export const LOG_TYPES = [
   'Syslog.RFC3164',
   'Syslog.RFC5424',
   'Zeek.DNS',
-  'Lacework',
+  'Lacework.Events',
 ] as const;
 
 export const PANTHER_SCHEMA_DOCS_MASTER_LINK = 'https://docs.runpanther.io';
