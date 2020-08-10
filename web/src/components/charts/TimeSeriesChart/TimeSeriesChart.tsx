e730be695a7a6c5f4fb2e1e5f4780d517d9853ac
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-08-10 16:58:09 +0300

    feat: update page icons

--- web/src/components/charts/TimeSeriesChart/TimeSeriesChart.tsx
+++ web/src/components/charts/TimeSeriesChart/TimeSeriesChart.tsx
@@ -177,6 +177,19 @@ const TimeSeriesChart: React.FC<TimeSeriesLinesProps> = ({
             fontFamily: theme.fonts.primary,
             fontSize: remToPx(theme.fontSizes['x-small']),
           },
+          pageIcons: {
+            vertical: ['M7 10L12 15L17 10H7Z', 'M7 14L12 9L17 14H7Z'],
+          },
+          pageIconColor: theme.colors['gray-50'],
+          pageIconInactiveColor: theme.colors['navyblue-300'],
+          pageIconSize: 12,
+          pageTextStyle: {
+            fontFamily: theme.fonts.primary,
+            color: theme.colors['gray-50'],
+            fontWeight: theme.fontWeights.bold as any,
+            fontSize: remToPx(theme.fontSizes['x-small']),
+          },
+          pageButtonGap: theme.space[3] as number,
         },
         xAxis: {
           type: 'time' as const,
