27c9d0e3938ad1821c3f99457532adde66adbc74
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-08-21 16:40:09 +0300

    test: simplify test cases

--- web/src/components/Navigation/NavLink/NavLink.test.tsx
+++ web/src/components/Navigation/NavLink/NavLink.test.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import { render } from 'test-utils';
-import urls from 'Source/urls';
 import NavLink from './index';
 
 describe('NavLink', () => {
   it('matches a URI regardless of hashes or query params', () => {
-    const url = urls.logAnalysis.alerts.list();
-    const { getByText } = render(<NavLink to={url} label="alerts" icon="list" />, {
-      initialRoute: `${url}#whatever?q=something`,
+    const { getByText } = render(<NavLink to="/something/" label="alerts" icon="list" />, {
+      initialRoute: '/something/#whatever?q=anything',
+    });
+
+    expect(getByText('alerts').closest('a')).toHaveAttribute('aria-current', 'page');
+  });
+
+  it('matches a URI regardless of trailing slashes', () => {
+    const { getByText } = render(<NavLink to="/whatever" label="alerts" icon="list" />, {
+      initialRoute: `/whatever/`,
     });
 
     expect(getByText('alerts').closest('a')).toHaveAttribute('aria-current', 'page');
   });
 
   it('matches children URIs', () => {
-    const { getByText } = render(
-      <NavLink to={urls.logAnalysis.alerts.list()} label="alerts" icon="list" />,
-      {
-        initialRoute: urls.logAnalysis.alerts.details('id'),
-      }
-    );
+    const { getByText } = render(<NavLink to="/something/" label="alerts" icon="list" />, {
+      initialRoute: '/something/particular/',
+    });
 
     expect(getByText('alerts').closest('a')).toHaveAttribute('aria-current', 'page');
   });
 
   it('ignores unrelated URIs', () => {
-    const { getByText } = render(
-      <NavLink to={urls.logAnalysis.alerts.list()} label="alerts" icon="list" />,
-      {
-        initialRoute: urls.compliance.sources.list(),
-      }
-    );
+    const { getByText } = render(<NavLink to="/something/" label="alerts" icon="list" />, {
+      initialRoute: '/something-else/',
+    });
 
     expect(getByText('alerts').closest('a')).not.toHaveAttribute('aria-current', 'page');
   });
