a778147bf86374c91d7e7ea8af0130d9a4535aad
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-08-31 21:23:58 +0300

    test: strengthen test  assertions

--- web/src/components/Navigation/NavLink/NavLink.test.tsx
+++ web/src/components/Navigation/NavLink/NavLink.test.tsx
@@ -17,39 +17,45 @@
  */
 
 import React from 'react';
-import { render } from 'test-utils';
+import { render, screen } from 'test-utils';
 import NavLink from './index';
 
 describe('NavLink', () => {
+  const navLinkDisplayName = 'alerts';
+  const getNavLink = () =>
+    screen.getByText((content, element) => {
+      return element.tagName.toLowerCase() === 'a' && element.textContent === navLinkDisplayName;
+    });
+
   it('matches a URI regardless of hashes or query params', () => {
-    const { getByText } = render(<NavLink to="/something/" label="alerts" icon="list" />, {
+    render(<NavLink to="/something/" label={navLinkDisplayName} icon="list" />, {
       initialRoute: '/something/#whatever?q=anything',
     });
 
-    expect(getByText('alerts').closest('a')).toHaveAttribute('aria-current', 'page');
+    expect(getNavLink()).toHaveAttribute('aria-current', 'page');
   });
 
   it('matches a URI regardless of trailing slashes', () => {
-    const { getByText } = render(<NavLink to="/whatever" label="alerts" icon="list" />, {
+    render(<NavLink to="/whatever" label={navLinkDisplayName} icon="list" />, {
       initialRoute: `/whatever/`,
     });
 
-    expect(getByText('alerts').closest('a')).toHaveAttribute('aria-current', 'page');
+    expect(getNavLink()).toHaveAttribute('aria-current', 'page');
   });
 
   it('matches children URIs', () => {
-    const { getByText } = render(<NavLink to="/something/" label="alerts" icon="list" />, {
+    render(<NavLink to="/something/" label={navLinkDisplayName} icon="list" />, {
       initialRoute: '/something/particular/',
     });
 
-    expect(getByText('alerts').closest('a')).toHaveAttribute('aria-current', 'page');
+    expect(getNavLink()).toHaveAttribute('aria-current', 'page');
   });
 
   it('ignores unrelated URIs', () => {
-    const { getByText } = render(<NavLink to="/something/" label="alerts" icon="list" />, {
+    render(<NavLink to="/something/" label={navLinkDisplayName} icon="list" />, {
       initialRoute: '/something-else/',
     });
 
-    expect(getByText('alerts').closest('a')).not.toHaveAttribute('aria-current', 'page');
+    expect(getNavLink()).not.toHaveAttribute('aria-current', 'page');
   });
 });
