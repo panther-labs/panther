4c73afddb74b379aea6b554c798d09ed3061497c
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-09-10 17:39:49 +0300

    fix: bypass focus lock

--- web/__tests__/__mocks__/@reach/dialog.tsx
+++ web/__tests__/__mocks__/@reach/dialog.tsx
@@ -21,7 +21,7 @@ import React from 'react';
 const { DialogOverlay, ...otherModules } = jest.requireActual('@reach/dialog');
 
 const MockedDialogOverlay: React.FC = props => (
-  <DialogOverlay {...props} dangerouslyBypassScrollLock />
+  <DialogOverlay {...props} dangerouslyBypassScrollLock dangerouslyBypassFocusLock />
 );
 
 module.exports = {
