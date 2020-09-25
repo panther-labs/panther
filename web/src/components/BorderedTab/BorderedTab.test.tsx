10e8a034109d2cbac7cbde021e84ce22c1356f27
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-09-25 13:43:35 +0300

    refactor: merge common tab code into a single `BorderedTab` component

---
+++ web/src/components/BorderedTab/BorderedTab.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render, fireEvent } from 'test-utils';
+import { TabList, TabPanel, TabPanels, Tabs } from 'pouncejs';
+import { BorderedTab } from './index';
+
+describe('BorderedTab', () => {
+  it('renders', () => {
+    const { container } = render(
+      <Tabs>
+        <TabList>
+          <BorderedTab>1</BorderedTab>
+          <BorderedTab>2</BorderedTab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>One</TabPanel>
+          <TabPanel>Two</TabPanel>
+        </TabPanels>
+      </Tabs>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('expect to work like a normal `Tab` element', () => {
+    const { getByText } = render(
+      <Tabs>
+        <TabList>
+          <BorderedTab>1</BorderedTab>
+          <BorderedTab>2</BorderedTab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>One</TabPanel>
+          <TabPanel>Two</TabPanel>
+        </TabPanels>
+      </Tabs>
+    );
+
+    expect(getByText('One')).toBeInTheDocument();
+    expect(getByText('Two')).not.toBeVisible();
+
+    fireEvent.click(getByText('2'));
+
+    expect(getByText('One')).not.toBeVisible();
+    expect(getByText('Two')).toBeInTheDocument();
+
+    fireEvent.keyDown(getByText('2'), { key: 'ArrowLeft', code: 'ArrowLeft' });
+
+    expect(getByText('One')).toBeInTheDocument();
+    expect(getByText('Two')).not.toBeVisible();
+  });
+});
