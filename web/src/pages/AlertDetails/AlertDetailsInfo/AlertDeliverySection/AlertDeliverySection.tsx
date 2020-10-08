b3004fd224075d7e5f6cac895b4586ec5c577c2a
Nick Angelou <nick.angelou@runpanther.io>
2020-10-08 12:40:10 -0500

    fix: PR feedback, disable resend button if loading

--- web/src/pages/AlertDetails/AlertDetailsInfo/AlertDeliverySection/AlertDeliverySection.tsx
+++ web/src/pages/AlertDetails/AlertDetailsInfo/AlertDeliverySection/AlertDeliverySection.tsx
@@ -21,6 +21,8 @@ import { Text, Flex, Icon, AbstractButton, Box, Collapse, useSnackbar } from 'po
 import { AlertDetails, ListDestinations } from 'Pages/AlertDetails';
 import last from 'lodash/last';
 import { DeliveryResponseFull } from 'Source/graphql/fragments/DeliveryResponseFull.generated';
+import groupBy from 'lodash/groupBy';
+import orderBy from 'lodash/orderBy';
 import AlertDeliveryTable from './AlertDeliveryTable';
 import { useRetryAlertDelivery } from './graphql/retryAlertDelivery.generated';
 
@@ -36,7 +38,7 @@ const AlertDeliverySection: React.FC<AlertDeliverySectionProps> = ({
   const [isHistoryVisible, setHistoryVisibility] = React.useState(false);
 
   const { pushSnackbar } = useSnackbar();
-  const [retryAlertDelivery] = useRetryAlertDelivery({
+  const [retryAlertDelivery, { loading }] = useRetryAlertDelivery({
     update: (cache, { data }) => {
       const dataId = cache.identify({
         __typename: 'AlertDetails',
@@ -98,38 +100,27 @@ const AlertDeliverySection: React.FC<AlertDeliverySectionProps> = ({
       </Flex>
     );
   }
-  // Need to determine success for each destination (group by destination).
-  const deliveryStatusByDestination = React.useMemo(() => {
-    return enhancedAndSortedAlertDeliveries.reduce((acc, dest: DeliveryResponseFull) => {
-      if (!acc[dest.outputId]) {
-        acc[dest.outputId] = [dest];
-        return acc;
-      }
-      acc[dest.outputId] = [...acc[dest.outputId], dest];
-      return acc;
-    }, {});
-  }, [enhancedAndSortedAlertDeliveries]);
 
-  // Next, we sort each status inside each group by dispatchedAt and determine if it was successful
-  // This is all or nothing. The most recent status for ALL destinations should be successful, otherwise
-  // notify the user of a failure.
   const allDestinationDeliveryStatuesSuccessful = React.useMemo(() => {
+    // Need to determine success for each destination (group by destination).
+    const deliveryStatusByDestination = groupBy(
+      enhancedAndSortedAlertDeliveries,
+      (d: DeliveryResponseFull) => d.outputId
+    );
+
+    // Next, we sort each status inside each group by dispatchedAt and determine if it was successful
+    // This is all or nothing. The most recent status for ALL destinations should be successful, otherwise
+    // notify the user of a failure.
     return Object.values(deliveryStatusByDestination).every((dest: Array<DeliveryResponseFull>) => {
       // We cant convert to date and compare because it would truncate
       // dispatchedAt to milliseconds, but they're often dispatched within
-      // a few nano seconds. Therefore, we define a comparator on strings
-      const sorted = dest.sort((a, b) => {
-        if (a.dispatchedAt > b.dispatchedAt) {
-          return -1;
-        }
-        if (b.dispatchedAt < a.dispatchedAt) {
-          return 1;
-        }
-        return 0;
-      });
+      // a few nano seconds. Therefore, we compare on strings.
+      const sorted = orderBy(dest, ['dispatchedAt'], ['desc']);
+      // Now that we've sorted the statues, the most recent status
+      // should indicate success or failure to the user.
       return sorted[0].success;
     });
-  }, [deliveryStatusByDestination]);
+  }, [enhancedAndSortedAlertDeliveries]);
 
   return (
     <Box>
@@ -161,6 +152,7 @@ const AlertDeliverySection: React.FC<AlertDeliverySectionProps> = ({
           <AlertDeliveryTable
             alertDeliveries={enhancedAndSortedAlertDeliveries}
             onAlertDeliveryRetry={onAlertDeliveryRetry}
+            isResending={loading}
           />
         </Box>
       </Collapse>
