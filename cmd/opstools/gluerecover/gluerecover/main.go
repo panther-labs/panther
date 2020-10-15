0cefbbf2ec978bf5dd73f2d356cec4db5b7cf19b
Alexandros Sigalas <alex.sigalas@runpanther.io>
2020-10-15 14:12:56 +0300

    Add flag to skip version check in gluesync/gluerecover

--- cmd/opstools/gluerecover/gluerecover/main.go
+++ cmd/opstools/gluerecover/gluerecover/main.go
@@ -42,28 +42,30 @@ var (
 func main() {
 	opstools.SetUsage("scans S3 for missing AWS Glue partitions and recovers them (Panther version %s)", version)
 	opts := struct {
-		MasterStack    *string
-		End            *string
-		Start          *string
-		DryRun         *bool
-		Debug          *bool
-		Region         *string
-		NumWorkers     *int
-		MaxConnections *int
-		MaxRetries     *int
-		Prefix         *string
+		MasterStack      *string
+		End              *string
+		Start            *string
+		DryRun           *bool
+		Debug            *bool
+		Region           *string
+		NumWorkers       *int
+		MaxConnections   *int
+		MaxRetries       *int
+		Prefix           *string
+		SkipVersionCheck *bool
 	}{
 		MasterStack: flag.String("master-stack", "",
 			"if set, this is the name of the Panther master stack used to deploy, if not set the deployment is assumed from source"),
-		Start:          flag.String("start", "", "Recover partitions after this date YYYY-MM-DD"),
-		End:            flag.String("end", "", "Recover partitions until this date YYYY-MM-DD"),
-		DryRun:         flag.Bool("dry-run", false, "Scan for missing partitions without without applying any changes"),
-		Debug:          flag.Bool("debug", false, "Enable additional logging"),
-		Region:         flag.String("region", "", "Set the AWS region to run on"),
-		MaxRetries:     flag.Int("max-retries", 12, "Max retries for AWS requests"),
-		MaxConnections: flag.Int("max-connections", 100, "Max number of connections to AWS"),
-		NumWorkers:     flag.Int("workers", 8, "Number of parallel workers for each table"),
-		Prefix:         flag.String("prefix", "", "A prefix to filter log type names"),
+		Start:            flag.String("start", "", "Recover partitions after this date YYYY-MM-DD"),
+		End:              flag.String("end", "", "Recover partitions until this date YYYY-MM-DD"),
+		DryRun:           flag.Bool("dry-run", false, "Scan for missing partitions without without applying any changes"),
+		Debug:            flag.Bool("debug", false, "Enable additional logging"),
+		Region:           flag.String("region", "", "Set the AWS region to run on"),
+		MaxRetries:       flag.Int("max-retries", 12, "Max retries for AWS requests"),
+		MaxConnections:   flag.Int("max-connections", 100, "Max number of connections to AWS"),
+		NumWorkers:       flag.Int("workers", 8, "Number of parallel workers for each table"),
+		Prefix:           flag.String("prefix", "", "A prefix to filter log type names"),
+		SkipVersionCheck: flag.Bool("skip-version-check", false, "Skip panther version check"),
 	}
 	flag.Parse()
 
@@ -98,7 +100,9 @@ func main() {
 		log.Fatalf("failed to build AWS session: %s", err)
 	}
 
-	opstools.ValidatePantherVersion(sess, log, *opts.MasterStack, version)
+	if !*opts.SkipVersionCheck {
+		opstools.ValidatePantherVersion(sess, log, *opts.MasterStack, version)
+	}
 
 	glueAPI := glue.New(sess)
 	s3API := s3.New(sess)
