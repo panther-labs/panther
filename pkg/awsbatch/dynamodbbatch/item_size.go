b75264ad8229a587af936a1f4719c4bbf161a58b
Nicholas Hakmiller <nicholas.hakmiller@runpanther.io>
2020-10-16 17:08:03 -0700

    unit tests

--- pkg/awsbatch/dynamodbbatch/item_size.go
+++ pkg/awsbatch/dynamodbbatch/item_size.go
@@ -19,7 +19,6 @@ package dynamodbbatch
  */
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
@@ -46,20 +45,20 @@ func getDynamoAttributeValueSize(value *dynamodb.AttributeValue) int {
 	if value.B != nil {
 		return len(value.B)
 	}
-	// Lists have 3 bytes of overhead
+	// Lists have 3 bytes of overhead for the list, and 1 byte of overhead per element
 	if value.L != nil {
-		size := 3
+		size := 3 + len(value.L)
 		for _, nestedValue := range value.L {
 			size += getDynamoAttributeValueSize(nestedValue)
 		}
 		return size
 	}
 	if value.S != nil {
-		return len(aws.StringValue(value.S))
+		return len(*value.S)
 	}
-	// Maps have 3 bytes of overhead
+	// Maps have 3 bytes of overhead, plus 1 byte of overhead per key/value pair
 	if value.M != nil {
-		return 3 + GetDynamoItemSize(value.M)
+		return 3 + GetDynamoItemSize(value.M) + len(value.M)
 	}
 	if value.BOOL != nil {
 		return 1
@@ -76,20 +75,35 @@ func getDynamoAttributeValueSize(value *dynamodb.AttributeValue) int {
 	//
 	// "Numbers are variable length, with up to 38 significant digits. Leading and trailing zeroes
 	// are trimmed. The size of a number is approximately
-	// (length of attribute name) + (1 byte per two significant digits) + (1 byte).
+	// (length of attribute name) + (1 byte per two significant digits) + (1 byte)."
+	//
+	// There is an additional 1 byte of overhead for negative numbers.
 	//
 	// So to estimate that approximation, I just divide the length by 2 (to approximate significant
 	// digits) and add 1.
 	//
-	// Additionally, dynamo rounds up on the significant digits / 2 math, so we add .5 then cast to
-	// an int to round up
+	// For odd numbers of significant digits, dynamo rounds up. That means a 7 digit number takes 4
+	// bytes (7/2 = 3.5 round up to 4). Since we're returning an int, though, we would truncate this
+	// to 3. To get around this, we always add .5 after dividing by 2. This means on odd digits such
+	// as 7 we get 7/2 = 3.5 + .5 = 4 (the same number dynamo would get) and on event digits such as
+	// 8 we get 8/2 = 4 = .5 = 4.5 cast to an integer = 4, which is again the correct value.
 	if value.N != nil {
-		return int(float64(len(aws.StringValue(value.N)))/2.0 + 1 + .5)
+		overhead := 1.5
+		if len(*value.N) > 0 && (*value.N)[0] == '-' {
+			overhead++
+		}
+		return int(float64(len(*value.N))/2 + overhead)
 	}
 	if value.NS != nil {
 		size := 0.0
 		for _, number := range value.NS {
-			size += float64(len(aws.StringValue(number))/2) + 1 + .5
+			if number != nil {
+				overhead := 1.5
+				if len(*number) > 0 && (*number)[0] == '-' {
+					overhead++
+				}
+				size += float64(len(*number))/2 + overhead
+			}
 		}
 		return int(size)
 	}
@@ -99,7 +113,9 @@ func getDynamoAttributeValueSize(value *dynamodb.AttributeValue) int {
 	if value.SS != nil {
 		size := 0
 		for _, stringValue := range value.SS {
-			size += len(aws.StringValue(stringValue))
+			if stringValue != nil {
+				size += len(*stringValue)
+			}
 		}
 		return size
 	}
