96171e5e114ef489eba97280a0baf4274e4defb6
Alexandros Sigalas <alex.sigalas@runpanther.io>
2020-10-23 17:34:30 +0300

    normalize gravitationallogs

--- internal/log_analysis/log_processor/parsers/gravitationallogs/gravitationallogs.go
+++ internal/log_analysis/log_processor/parsers/gravitationallogs/gravitationallogs.go
@@ -20,8 +20,6 @@ package gravitationallogs
 
 import "github.com/panther-labs/panther/internal/log_analysis/log_processor/logtypes"
 
-const TypeTeleportAudit = "Gravitational.TeleportAudit"
-
 // LogTypes exports all available logtypes in this group
 func LogTypes() logtypes.Group {
 	return logTypes
@@ -30,7 +28,7 @@ func LogTypes() logtypes.Group {
 var logTypes = logtypes.Must("Gravitational",
 	// nolint:lll
 	logtypes.ConfigJSON{
-		Name:         TypeTeleportAudit,
+		Name:         "Gravitational.TeleportAudit",
 		Description:  `Teleport logs events like successful user logins along with the metadata like remote IP address, time and the session ID.`,
 		ReferenceURL: `https://gravitational.com/teleport/docs/admin-guide/#audit-log`,
 		NewEvent: func() interface{} {
