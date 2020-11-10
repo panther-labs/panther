b46d23fbd6d444438680c4a2b9be5a347419244f
Austin Byers <austin.byers@runpanther.io>
2020-11-10 11:20:20 -0800

    Remove remediation-api gateway

--- internal/compliance/remediation_api/handlers/apihandlers.go
+++ internal/compliance/remediation_api/handlers/apihandlers.go
@@ -1,4 +1,4 @@
-package apihandlers
+package handlers
 
 /**
  * Panther is a Cloud-Native SIEM for the Modern Security Team.
@@ -19,18 +19,12 @@ package apihandlers
  */
 
 import (
-	"net/http"
 	"os"
 
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/aws/aws-sdk-go/service/sqs/sqsiface"
-	jsoniter "github.com/json-iterator/go"
-	"go.uber.org/zap"
 
-	"github.com/panther-labs/panther/api/gateway/remediation/models"
 	"github.com/panther-labs/panther/internal/compliance/remediation_api/remediation"
 )
 
@@ -42,16 +36,19 @@ var (
 	invoker    remediation.InvokerAPI = remediation.NewInvoker(session.Must(session.NewSession()))
 
 	//RemediationLambdaNotFound is the Error when the remediation Lambda is not found
-	RemediationLambdaNotFound = &models.Error{Message: aws.String("Remediation Lambda not found or misconfigured")}
+	RemediationLambdaNotFound = "Remediation Lambda not found or misconfigured"
 )
 
-func badRequest(errorMessage *string) *events.APIGatewayProxyResponse {
-	errModel := &models.Error{Message: errorMessage}
-	body, err := jsoniter.MarshalToString(errModel)
-	if err != nil {
-		zap.L().Error("errModel.MarshalBinary failed", zap.Error(err))
-		body = "invalid request"
-		return &events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest, Body: body}
-	}
-	return &events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest, Body: body}
-}
+// API has all of the handlers as receiver methods
+type API struct{}
+
+//func badRequest(errorMessage *string) *events.APIGatewayProxyResponse {
+//	errModel := &models.Error{Message: errorMessage}
+//	body, err := jsoniter.MarshalToString(errModel)
+//	if err != nil {
+//		zap.L().Error("errModel.MarshalBinary failed", zap.Error(err))
+//		body = "invalid request"
+//		return &events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest, Body: body}
+//	}
+//	return &events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest, Body: body}
+//}
