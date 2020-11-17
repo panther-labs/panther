b3365d748d590a7458f8e50ee1dedac4771fda93
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-11-17 11:20:38 +0200

    fix: opsgenie form

--- web/src/components/forms/OpsgenieDestinationForm/OpsgenieDestinationForm.tsx
+++ web/src/components/forms/OpsgenieDestinationForm/OpsgenieDestinationForm.tsx
@@ -26,14 +26,11 @@ import BaseDestinationForm, {
   BaseDestinationFormValues,
   defaultValidationSchema,
 } from 'Components/forms/BaseDestinationForm';
-import { Box, Flex, FormHelperText, SimpleGrid } from 'pouncejs';
+import { Flex, Text, SimpleGrid } from 'pouncejs';
 import FormikRadio from 'Components/fields/Radio';
 
 type OpsgenieFieldValues = Pick<DestinationConfigInput, 'opsgenie'>;
-type RegionFieldName = {
-  US: string;
-  EU: string;
-};
+
 interface OpsgenieDestinationFormProps {
   initialValues: BaseDestinationFormValues<OpsgenieFieldValues>;
   onSubmit: (values: BaseDestinationFormValues<OpsgenieFieldValues>) => void;
@@ -43,14 +40,6 @@ const OpsgenieDestinationForm: React.FC<OpsgenieDestinationFormProps> = ({
   onSubmit,
   initialValues,
 }) => {
-  const [serviceRegion, setServiceRegion] = React.useState<OpsgenieServiceRegionEnum>(
-    OpsgenieServiceRegionEnum.Eu
-  );
-  const [fieldNames, setFieldNames] = React.useState<RegionFieldName>({
-    US: 'outputConfig.opsgenie.serviceRegion.US',
-    EU: 'outputConfig.opsgenie.serviceRegion',
-  });
-
   const existing = initialValues.outputId;
 
   const opsgenieFieldsValidationSchema = Yup.object().shape({
@@ -63,17 +52,6 @@ const OpsgenieDestinationForm: React.FC<OpsgenieDestinationFormProps> = ({
   });
   const mergedValidationSchema = defaultValidationSchema.concat(opsgenieFieldsValidationSchema);
 
-  const handleSelectChange = (region: OpsgenieServiceRegionEnum) => {
-    setFieldNames(prevState => ({
-      ...prevState,
-      [region]: 'outputConfig.opsgenie.serviceRegion',
-    }));
-    setServiceRegion(region);
-  };
-
-  // console.log('fieldNames', fieldNames)
-  // console.log('serviceRegion', serviceRegion)
-  // console.log('initialValues', initialValues.outputConfig.opsgenie.serviceRegion)
   return (
     <BaseDestinationForm<OpsgenieFieldValues>
       initialValues={initialValues}
@@ -97,32 +75,24 @@ const OpsgenieDestinationForm: React.FC<OpsgenieDestinationFormProps> = ({
           required={!existing}
           autoComplete="new-password"
         />
-        <Box fontSize="medium" fontWeight="medium" flexGrow={1} textAlign="left">
-          <FormHelperText mt={2} id="serviceRegion-helper-text">
-            Change this selection to EU if you are registered to Opsgenie Europe
-            (app.eu.opsgenie.com).
-          </FormHelperText>
-        </Box>
-        <Flex align="center" justify="space-between">
+        <Text fontSize="small-medium" id="serviceRegion-label-text" color="gray-300" my={1}>
+          Change this selection to EU if you are registered to Opsgenie Europe
+          (app.eu.opsgenie.com).
+        </Text>
+        <Flex align="flex-start" justify="space-between">
           <FastField
-            key={'test1'}
             as={FormikRadio}
-            name={fieldNames.US}
-            onChange={() => handleSelectChange(OpsgenieServiceRegionEnum.Us)}
-            // checked={selectedSupportRegion === OpsgenieServiceRegionEnum.Eu}
-            value={serviceRegion}
+            name="serviceRegion"
+            value={OpsgenieServiceRegionEnum.Us}
             label="US Service Region"
-            aria-describedby="serviceRegion-helper-text"
+            aria-describedby="serviceRegion-label-text"
           />
           <FastField
-            key={'test2'}
             as={FormikRadio}
-            name={fieldNames.EU}
-            onChange={() => handleSelectChange(OpsgenieServiceRegionEnum.Eu)}
-            // checked={selectedSupportRegion === OpsgenieServiceRegionEnum.Eu}
-            value={serviceRegion}
+            name="serviceRegion"
+            value={OpsgenieServiceRegionEnum.Eu}
             label="EU Service Region"
-            aria-describedby="serviceRegion-helper-text"
+            aria-describedby="serviceRegion-label-text"
           />
         </Flex>
       </SimpleGrid>
