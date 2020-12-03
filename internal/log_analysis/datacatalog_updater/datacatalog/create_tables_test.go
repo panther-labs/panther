fa75a0e3403b976247c940a738c7d0cf917cfb62
Kostas Papageorgiou <kostas@runpanther.io>
2020-12-03 16:49:35 +0200

    Fixing syncing

--- internal/log_analysis/datacatalog_updater/datacatalog/create_tables_test.go
+++ internal/log_analysis/datacatalog_updater/datacatalog/create_tables_test.go
@@ -109,8 +109,8 @@ func TestSQS_Sync(t *testing.T) {
 	}, nil)
 	mockAthenaClient.On("GetQueryResults", mock.Anything).Return(&athena.GetQueryResultsOutput{}, nil)
 
-	// Sync
-	mockSqsClient.On("SendMessageWithContext", mock.Anything, mock.Anything).Return(&sqs.SendMessageOutput{}, nil).Once()
+	// Two sync messages, one for the Cloud Security data and another for the Log data
+	mockSqsClient.On("SendMessageWithContext", mock.Anything, mock.Anything).Return(&sqs.SendMessageOutput{}, nil).Twice()
 
 	err = handler.HandleSQSEvent(context.Background(), &event)
 	require.NoError(t, err)
