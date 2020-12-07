707cd3aeb67cc9fd78050a90b49da996f3774fe1
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-12-07 12:30:57 +0200

    fix: conflict

--- web/src/components/GenericItemCard/GenericItemCard.tsx
+++ web/src/components/GenericItemCard/GenericItemCard.tsx
@@ -120,11 +120,7 @@ const GenericItemCardOptionsButton = React.forwardRef<HTMLButtonElement>(functio
 
 const GenericItemCardDate: React.FC<GenericItemCardDate> = ({ date, ...rest }) => {
   return (
-<<<<<<< HEAD
-    <Text fontSize="small" as="span" color="gray-500" {...rest}>
-=======
     <Text fontSize="small" as="span" color="navyblue-100" {...rest}>
->>>>>>> 067184d6 (Fix: Empty charts & Buggy log scale (#2195))
       {date}
     </Text>
   );
