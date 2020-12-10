5f7d5c00322b1547737a2e08f6428add30465285
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-12-10 14:35:12 +0200

    fix: update tests & snaps

--- web/src/pages/ListAlerts/ListAlerts.test.tsx
+++ web/src/pages/ListAlerts/ListAlerts.test.tsx
@@ -669,7 +669,7 @@ describe('ListAlerts', () => {
     await waitMs(1);
 
     // Expect the URL to be updated
-    const completeParams = `${paramsWithSortingAndTextFilter}&createdAtAfter=2000-01-29T00:00:00Z&createdAtBefore=2000-01-30T00:00:00Z`;
+    const completeParams = `${paramsWithSortingAndTextFilter}&createdAtAfter=2000-01-29T00:00:00.000Z&createdAtBefore=2000-01-30T00:00:00.000Z`;
     expect(parseParams(history.location.search)).toEqual(parseParams(completeParams));
 
     // Expect the API request to have fired and a new alert to have returned (verifies API execution)
