9e90cd94dbb794d15717e0af39b93ffcc2b56569
George Papadrosou <george.papadrosou@runpanther.io>
2020-12-16 16:49:48 +0200

    Add indicators

--- internal/log_analysis/log_processor/parsers/slacklogs/accesslogs.go
+++ internal/log_analysis/log_processor/parsers/slacklogs/accesslogs.go
@@ -25,7 +25,7 @@ const TypeAccessLogs = "Slack.AccessLogs"
 //nolint:lll
 type AccessLog struct {
 	UserID    pantherlog.String `json:"user_id" validate:"required" description:"The id of the user accessing Slack."`
-	UserName  pantherlog.String `json:"username" description:"The username of the user accessing Slack."`
+	UserName  pantherlog.String `json:"username" panther:"username" description:"The username of the user accessing Slack."`
 	DateFirst pantherlog.Time   `json:"date_first" validate:"required" tcodec:"unix" description:"Unix timestamp of the first access log entry for this user, IP address, and user agent combination."`
 	DateLast  pantherlog.Time   `json:"date_last" validate:"required" tcodec:"unix" event_time:"true" description:"Unix timestamp of the most recent access log entry for this user, IP address, and user agent combination."`
 	Count     pantherlog.Int32  `json:"count" validate:"required" description:"The total number of access log entries for that combination."`
--- internal/log_analysis/log_processor/parsers/slacklogs/auditlogs.go
+++ internal/log_analysis/log_processor/parsers/slacklogs/auditlogs.go
@@ -28,9 +28,9 @@ const TypeAuditLogs = "Slack.AuditLogs"
 
 // nolint:lll
 type AuditLog struct {
-	ID         string               `json:"id" validate:"required" description:"The event id"`
+	ID         pantherlog.String    `json:"id" validate:"required" description:"The event id"`
 	DateCreate pantherlog.Time      `json:"date_create" validate:"required" tcodec:"unix" event_time:"true" description:"Creation timestamp for the event"`
-	Action     string               `json:"action" validate:"required" description:"The action performed. See https://api.slack.com/enterprise/audit-logs#audit_logs_actions"`
+	Action     pantherlog.String    `json:"action" validate:"required" description:"The action performed. See https://api.slack.com/enterprise/audit-logs#audit_logs_actions"`
 	Actor      Actor                `json:"actor" validate:"required" description:"An actor will always be a user on a workspace and will be identified by their user ID, such as W123AB456."`
 	Entity     Entity               `json:"entity" validate:"required" description:"An entity is the thing that the actor has taken the action upon and it will be the Slack ID of the thing."`
 	Context    Context              `json:"context" validate:"required" description:"Context is the location that the actor took the action on the entity. It will always be either a Workspace or an Enterprise, with the appropriate ID."`
@@ -39,97 +39,97 @@ type AuditLog struct {
 
 // nolint:lll
 type Entity struct {
-	Type       string      `json:"type" validate:"required" description:"The type of item that was affected by the action (user,channel,file,app,workspace,enterprise,message,workflow)"`
-	User       *User       `json:"user,omitempty" description:"Information about the affected user"`
-	Channel    *Channel    `json:"channel,omitempty" description:"Information about the affected channel"`
-	File       *File       `json:"file,omitempty" description:"Information about the affected file"`
-	App        *App        `json:"app,omitempty" description:"Information about the affected app"`
-	Workspace  *Workspace  `json:"workspace,omitempty" description:"Information about the affected workspace"`
-	Enterprise *Enterprise `json:"enterprise,omitempty" description:"Information about the affected enterprise"`
-	Workflow   *Workflow   `json:"workflow,omitempty" description:"Information about the affected workflow"`
-	Message    *Message    `json:"message,omitempty" description:"Information about the affected message"`
+	Type       pantherlog.String `json:"type" validate:"required" description:"The type of item that was affected by the action (user,channel,file,app,workspace,enterprise,message,workflow)"`
+	User       *User             `json:"user,omitempty" description:"Information about the affected user"`
+	Channel    *Channel          `json:"channel,omitempty" description:"Information about the affected channel"`
+	File       *File             `json:"file,omitempty" description:"Information about the affected file"`
+	App        *App              `json:"app,omitempty" description:"Information about the affected app"`
+	Workspace  *Workspace        `json:"workspace,omitempty" description:"Information about the affected workspace"`
+	Enterprise *Enterprise       `json:"enterprise,omitempty" description:"Information about the affected enterprise"`
+	Workflow   *Workflow         `json:"workflow,omitempty" description:"Information about the affected workflow"`
+	Message    *Message          `json:"message,omitempty" description:"Information about the affected message"`
 }
 
 // nolint:lll
 type Actor struct {
-	Type string `json:"type" validate:"required,eq=user" description:"The type of actor (always user)"`
-	User User   `json:"user" description:"Information about the user"`
+	Type pantherlog.String `json:"type" validate:"required,eq=user" description:"The type of actor (always user)"`
+	User User              `json:"user" description:"Information about the user"`
 }
 
 // nolint:lll
 type User struct {
-	ID    string `json:"id" validate:"required" description:"The id of the user ('USLACKUSER' if no user performed the action)"`
-	Name  string `json:"name,omitempty" description:"The user's display name"`
-	Email string `json:"email,omitempty" description:"The user's email"`
-	Team  string `json:"team,omitempty" description:"The user's team"`
+	ID    pantherlog.String `json:"id" validate:"required" description:"The id of the user ('USLACKUSER' if no user performed the action)"`
+	Name  pantherlog.String `json:"name,omitempty" panther:"username" description:"The user's display name"`
+	Email pantherlog.String `json:"email,omitempty" panther:"email" description:"The user's email"`
+	Team  pantherlog.String `json:"team,omitempty" description:"The user's team"`
 }
 
 // nolint:lll
 type File struct {
-	ID       string `json:"id" validate:"required" description:"The id of the file"`
-	Name     string `json:"name,omitempty" description:"The filename"`
-	Title    string `json:"title,omitempty" description:"The file title"`
-	Filetype string `json:"filetype,omitempty" description:"The filetype"`
+	ID       pantherlog.String `json:"id" validate:"required" description:"The id of the file"`
+	Name     pantherlog.String `json:"name,omitempty" description:"The filename"`
+	Title    pantherlog.String `json:"title,omitempty" description:"The file title"`
+	Filetype pantherlog.String `json:"filetype,omitempty" description:"The filetype"`
 }
 
 // nolint:lll
 type Channel struct {
-	ID          string   `json:"id" validate:"required" description:"The id of the channel"`
-	Name        string   `json:"name,omitempty" description:"The name of the channel"`
-	Privacy     string   `json:"privacy,omitempty" description:"The privacy mode of the channel"`
-	Shared      bool     `json:"is_shared,omitempty" description:"Whether the channel is shared"`
-	OrgShared   bool     `json:"is_org_shared,omitempty" description:"Whether the channel is shared in the organisation"`
-	SharedTeams []string `json:"teams_shared_with,omitempty" description:"The teams the channel is shared with"`
+	ID          pantherlog.String   `json:"id" validate:"required" description:"The id of the channel"`
+	Name        pantherlog.String   `json:"name,omitempty" description:"The name of the channel"`
+	Privacy     pantherlog.String   `json:"privacy,omitempty" description:"The privacy mode of the channel"`
+	Shared      pantherlog.Bool     `json:"is_shared,omitempty" description:"Whether the channel is shared"`
+	OrgShared   pantherlog.Bool     `json:"is_org_shared,omitempty" description:"Whether the channel is shared in the organisation"`
+	SharedTeams []pantherlog.String `json:"teams_shared_with,omitempty" description:"The teams the channel is shared with"`
 }
 
 // nolint:lll
 type App struct {
-	ID                string   `json:"id" validate:"required" description:"The id of the app"`
-	Name              string   `json:"name,omitempty" description:"The name of the app"`
-	Distributed       bool     `json:"is_distributed,omitempty" description:"Whether the app is distributed"`
-	DirectoryApproved bool     `json:"is_directory_approved,omitempty" description:"Whether the app is in the approved apps directory"`
-	Scopes            []string `json:"scopes,omitempty" description:"The OAuth2 scopes the app requires"`
+	ID                pantherlog.String   `json:"id" validate:"required" description:"The id of the app"`
+	Name              pantherlog.String   `json:"name,omitempty" description:"The name of the app"`
+	Distributed       pantherlog.Bool     `json:"is_distributed,omitempty" description:"Whether the app is distributed"`
+	DirectoryApproved pantherlog.Bool     `json:"is_directory_approved,omitempty" description:"Whether the app is in the approved apps directory"`
+	Scopes            []pantherlog.String `json:"scopes,omitempty" description:"The OAuth2 scopes the app requires"`
 }
 
 // nolint:lll
 type Workspace struct {
-	ID     string `json:"id" validate:"required" description:"The id of the workspace"`
-	Name   string `json:"name,omitempty" description:"The name of the workspace"`
-	Domain string `json:"domain,omitempty" description:"The workspace domain"`
+	ID     pantherlog.String `json:"id" validate:"required" description:"The id of the workspace"`
+	Name   pantherlog.String `json:"name,omitempty" description:"The name of the workspace"`
+	Domain pantherlog.String `json:"domain,omitempty" description:"The workspace domain"`
 }
 
 // nolint:lll
 type Workflow struct {
-	ID   string `json:"id" validate:"required" description:"The id of the workflow"`
-	Name string `json:"name,omitempty" description:"The name of the workflow"`
+	ID   pantherlog.String `json:"id" validate:"required" description:"The id of the workflow"`
+	Name pantherlog.String `json:"name,omitempty" description:"The name of the workflow"`
 }
 
 // nolint:lll
 type Message struct {
-	Team    string `json:"team,omitempty" description:"The team the message was posted in"`
-	Channel string `json:"channel,omitempty" description:"The channel the message was posted on"`
+	Team    pantherlog.String `json:"team,omitempty" description:"The team the message was posted in"`
+	Channel pantherlog.String `json:"channel,omitempty" description:"The channel the message was posted on"`
 	// TODO: Get samples to find the format
-	Timestamp string `json:"timestamp,omitempty" description:"The timestamp of the message"`
+	Timestamp pantherlog.String `json:"timestamp,omitempty" description:"The timestamp of the message"`
 }
 
 // nolint:lll
 type Enterprise struct {
-	ID     string `json:"id" validate:"required" description:"The id of the enterprise"`
-	Name   string `json:"name,omitempty" description:"The name of the enterprise"`
-	Domain string `json:"domain,omitempty" description:"The enterprise domain"`
+	ID     pantherlog.String `json:"id" validate:"required" description:"The id of the enterprise"`
+	Name   pantherlog.String `json:"name,omitempty" description:"The name of the enterprise"`
+	Domain pantherlog.String `json:"domain,omitempty" description:"The enterprise domain"`
 }
 
 // nolint:lll
 type Context struct {
-	UserAgent string   `json:"ua,omitempty" description:"The user agent used for the action"`
-	IPAddress string   `json:"ip_address,omitempty" description:"The ip address the action was performed from"`
-	Location  Location `json:"location" description:"The location that the actor took the action on the entity."`
+	UserAgent pantherlog.String `json:"ua,omitempty" description:"The user agent used for the action"`
+	IPAddress pantherlog.String `json:"ip_address,omitempty" panther:"ip" description:"The ip address the action was performed from"`
+	Location  Location          `json:"location" description:"The location that the actor took the action on the entity."`
 }
 
 // nolint:lll
 type Location struct {
-	Type   string `json:"type" validate:"required" description:"The location type. It will always be either a Workspace or an Enterprise"`
-	ID     string `json:"id" validate:"required" description:"The location id"`
-	Domain string `json:"domain,omitempty" description:"The location domain"`
-	Name   string `json:"name,omitempty" description:"The location name"`
+	Type   pantherlog.String `json:"type" validate:"required" description:"The location type. It will always be either a Workspace or an Enterprise"`
+	ID     pantherlog.String `json:"id" validate:"required" description:"The location id"`
+	Domain pantherlog.String `json:"domain,omitempty" description:"The location domain"`
+	Name   pantherlog.String `json:"name,omitempty" description:"The location name"`
 }
