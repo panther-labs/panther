33e2dda9264ffda526d10f1c9ce34b0e86293158
tatehanawalt <tate.hanawalt@runpanther.io>
2021-01-07 16:36:36 -0600

    altered kv table read/write capacity selection parameter to KvTableBillingMode with values PROVISIONED, PAY_PER_USE

--- tools/mage/deploy/config.go
+++ tools/mage/deploy/config.go
@@ -39,7 +39,10 @@ type Infra struct {
 	LogProcessorLambdaMemorySize       int      `yaml:"LogProcessorLambdaMemorySize"`
 	LogProcessorLambdaSQSReadBatchSize string   `yaml:"LogProcessorLambdaSQSReadBatchSize"`
 	PipLayer                           []string `yaml:"PipLayer"`
-	ProvisionKvTable                   bool     `yaml:"ProvisionKvTable"`
+
+	// ProvisionKvTable                   bool     `yaml:"ProvisionKvTable"`
+	KvTableBillingMode                 string   `yaml:"KvTableBillingMode"`
+
 	PythonLayerVersionArn              string   `yaml:"PythonLayerVersionArn"`
 	SecurityGroupID                    string   `yaml:"SecurityGroupID"`
 	SubnetOneIPRange                   string   `yaml:"SubnetOneIPRange"`
