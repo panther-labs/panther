b04de1662b4fc889402aa1d5e570dbeea408fb21
lindsey-w <lindsey.whitehurst@runpanther.io>
2021-01-26 17:19:30 -0800

    move pack stuff to its own util

--- internal/core/analysis_api/handlers/util.go
+++ internal/core/analysis_api/handlers/util.go
@@ -238,57 +238,6 @@ func writeItem(item *tableItem, userID string, mustExist *bool) (int, error) {
 	return changeType, nil
 }
 
-// Create/update a pack
-//
-// The following fields are set automatically (need not be set by the caller):
-//     CreatedAt, CreatedBy, LastModified, LastModifiedBy, VersionID
-//
-// To update an existing item,              mustExist = aws.Bool(true)
-// To create a new item (with a unique ID), mustExist = aws.Bool(false)
-// To allow either an update or a create,   mustExist = nil (neither)
-//
-// The first return value indicates what kind of change took place (none, new item, updated item).
-func writePack(item *packTableItem, userID string, mustExist *bool) error {
-	oldItem, err := dynamoGetPack(item.ID, true)
-	if err != nil {
-		return err
-	}
-
-	if mustExist != nil {
-		if *mustExist && oldItem == nil {
-			return errNotExists // item should exist but does not (update)
-		}
-		if !*mustExist && oldItem != nil {
-			return errExists // item exists but should not (create)
-		}
-	}
-
-	if oldItem == nil {
-		item.CreatedAt = time.Now()
-		item.CreatedBy = userID
-	} else {
-		if equal := !packUpdated(oldItem, item); equal {
-			zap.L().Info("no changes necessary", zap.String("packId", item.ID))
-			return nil
-		}
-		// If there was an error evaluating equality, just assume they are not equal and continue
-		// with the update as normal.
-
-		item.CreatedAt = oldItem.CreatedAt
-		item.CreatedBy = oldItem.CreatedBy
-	}
-
-	item.LastModified = time.Now()
-	item.LastModifiedBy = userID
-
-	// Write to Dynamo
-	if err := dynamoPutPack(item); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // itemUpdated checks if ANY field has been changed between the old and new item. Only used to inform users whether the
 // result of a BulkUpload operation actually changed something or not.
 //
@@ -382,25 +331,6 @@ func mappingEquality(oldItem, newItem *tableItem) bool {
 		}
 	}
 	return true
-}
-
-// packUpdated checks if ANY field has been changed between the old and new pack item
-//
-// DO NOT use this for situations the items MUST be exactly equal, this is a "good enough" approximation for the
-// purpose it serves, which is informing users that their bulk operation did or did not change something.
-func packUpdated(oldItem, newItem *packTableItem) bool {
-	itemsEqual := oldItem.Description == newItem.Description &&
-		oldItem.DisplayName == newItem.DisplayName &&
-		oldItem.Enabled == newItem.Enabled &&
-		oldItem.EnabledVersion.ID == newItem.EnabledVersion.ID &&
-		oldItem.UpdateAvailable == newItem.UpdateAvailable &&
-		len(oldItem.AvailableVersions) == len(newItem.AvailableVersions)
-	if !itemsEqual {
-		return true
-	}
-	// check detection patterns, which currently only cover IDs
-	itemsEqual = itemsEqual && !setEquality(oldItem.DetectionPattern.IDs, newItem.DetectionPattern.IDs)
-	return !itemsEqual
 }
 
 // Sort a slice of strings ignoring case when possible
