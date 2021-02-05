50e55b59ff6d5482d28110db1502aa44b5ef7f5e
Kostas Papageorgiou <kostas@runpanther.io>
2021-02-05 15:10:41 +0200

    Removed space

--- internal/log_analysis/log_processor/parsers/gcplogs/auditlog.go
+++ internal/log_analysis/log_processor/parsers/gcplogs/auditlog.go
@@ -98,7 +98,7 @@ type AuditLog struct {
 	MethodName         *string              `json:"methodName,omitempty" description:"The name of the service method or operation. For API calls, this should be the name of the API method."`
 	ResourceName       *string              `json:"resourceName,omitempty" description:"The resource or collection that is the target of the operation. The name is a scheme-less URI, not including the API service name."`
 	NumResponseItems   *numerics.Int64      `json:"numResponseItems,omitempty" description:"The number of items returned from a List or Query API method, if applicable."`
-	Status             *Status              `json:"status,omitempty" description:" The status of the overall operation."`
+	Status             *Status              `json:"status,omitempty" description:"The status of the overall operation."`
 	AuthenticationInfo *AuthenticationInfo  `json:"authenticationInfo,omitempty" description:"Authentication information."`
 	AuthorizationInfo  []AuthorizationInfo  `json:"authorizationInfo,omitempty" validate:"omitempty,dive" description:"Authorization information. If there are multiple resources or permissions involved, then there is one AuthorizationInfo element for each {resource, permission} tuple."`
 	RequestMetadata    *RequestMetadata     `json:"requestMetadata,omitempty" description:"Metadata about the request"`
