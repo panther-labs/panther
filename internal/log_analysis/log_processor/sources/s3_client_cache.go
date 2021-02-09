188dc55592cfd62ce5f2b36817e48bade5a5cacc
Kostas Papageorgiou <kostas@runpanther.io>
2021-02-09 16:02:59 +0200

    WIP

--- internal/log_analysis/log_processor/sources/s3_client_cache.go
+++ internal/log_analysis/log_processor/sources/s3_client_cache.go
@@ -19,8 +19,6 @@ package sources
  */
 
 import (
-	"sort"
-	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -64,101 +62,6 @@ type s3ClientCacheKey struct {
 type prefixSource struct {
 	prefix string
 	source *models.SourceIntegration
-}
-
-type sourceCache struct {
-	// last time the cache was updated
-	cacheUpdateTime time.Time
-	// sources by id
-	index map[string]*models.SourceIntegration
-	// sources by s3 bucket sorted by longest prefix first
-	byBucket map[string][]prefixSource
-}
-
-// LoadS3 loads the source configuration for an S3 object.
-// This will update the cache if needed.
-// It will return error if it encountered an issue retrieving the source information
-func (c *sourceCache) LoadS3(bucketName, objectKey string) (*models.SourceIntegration, error) {
-	if err := c.Sync(time.Now()); err != nil {
-		return nil, err
-	}
-	return c.FindS3(bucketName, objectKey), nil
-}
-
-// Loads the source configuration for an source id.
-// This will update the cache if needed.
-// It will return error if it encountered an issue retrieving the source information or if the source is not found.
-func (c *sourceCache) Load(id string) (*models.SourceIntegration, error) {
-	if err := c.Sync(time.Now()); err != nil {
-		return nil, err
-	}
-	src := c.Find(id)
-	if src != nil {
-		return src, nil
-	}
-	return nil, errors.Errorf("source %q not found", id)
-}
-
-// Sync will update the cache if too much time has passed
-func (c *sourceCache) Sync(now time.Time) error {
-	if c.cacheUpdateTime.Add(sourceCacheDuration).Before(now) {
-		// we need to update the cache
-		input := &models.LambdaInput{
-			ListIntegrations: &models.ListIntegrationsInput{},
-		}
-		var output []*models.SourceIntegration
-		if err := genericapi.Invoke(common.LambdaClient, sourceAPIFunctionName, input, &output); err != nil {
-			return err
-		}
-		c.Update(now, output)
-	}
-	return nil
-}
-
-// Update updates the cache
-func (c *sourceCache) Update(now time.Time, sources []*models.SourceIntegration) {
-	byBucket := make(map[string][]prefixSource)
-	index := make(map[string]*models.SourceIntegration)
-	for _, source := range sources {
-		bucket, prefixes := source.S3Info()
-		for _, prefix := range prefixes {
-			byBucket[bucket] = append(byBucket[bucket], prefixSource{prefix: prefix, source: source})
-		}
-		index[source.IntegrationID] = source
-	}
-	// Sort sources for each bucket.
-	// It is important to have the sources sorted by longest prefix first.
-	// This ensures that longer prefixes (ie `/foo/bar`) have precedence over shorter ones (ie `/foo`).
-	// This is especially important for the empty prefix as it would match all objects in a bucket making
-	// other sources invalid.
-	for _, sources := range byBucket {
-		sources := sources
-		sort.Slice(sources, func(i, j int) bool {
-			// Sort by prefix length descending
-			return len(sources[i].prefix) > len(sources[j].prefix)
-		})
-	}
-	*c = sourceCache{
-		byBucket:        byBucket,
-		index:           index,
-		cacheUpdateTime: now,
-	}
-}
-
-// Find looks up a source by id without updating the cache
-func (c *sourceCache) Find(id string) *models.SourceIntegration {
-	return c.index[id]
-}
-
-// FindS3 looks up a source by bucket name and prefix without updating the cache
-func (c *sourceCache) FindS3(bucketName, objectKey string) *models.SourceIntegration {
-	prefixSourcesOrdered := c.byBucket[bucketName]
-	for _, s := range prefixSourcesOrdered {
-		if strings.HasPrefix(objectKey, s.prefix) {
-			return s.source
-		}
-	}
-	return nil
 }
 
 var (
