d088ab87a4e2f47898eeacd595f9c1e1aa8e9407
Alex Mylonas <alex.a.mylonas@gmail.com>
2021-02-10 15:12:05 +0200

    fix: reinstated logic from master

--- web/src/components/wizards/S3LogSourceWizard/S3SourceConfigurationPanel/S3SourceConfigurationPanel.tsx
+++ web/src/components/wizards/S3LogSourceWizard/S3SourceConfigurationPanel/S3SourceConfigurationPanel.tsx
@@ -34,6 +34,20 @@ const S3SourceConfigurationPanel: React.FC = () => {
     onError: () => pushSnackbar({ title: "Couldn't fetch your available log types" }),
   });
 
+  const shouldSkipCFNUpload = React.useMemo(() => {
+    return (
+      /*
+       * If users dont change any of AWS accountId, stackName or Integration label
+       * then users dont really need to update their stack or template, so we can skip the next step
+       * or proceed to validation.
+       * This will apply only to editing since creation requires users to change those field to proceed
+       */
+      initialValues.integrationLabel === values.integrationLabel &&
+      initialValues.s3Bucket === values.s3Bucket &&
+      initialValues.kmsKey === values.kmsKey
+    );
+  }, [initialValues, values]);
+
   // The filtering here is used to prevent users from adding the same log type with different prefixes
   const availableLogTypes = React.useMemo(() => {
     return data?.listAvailableLogTypes.logTypes ?? [];
@@ -153,7 +167,11 @@ const S3SourceConfigurationPanel: React.FC = () => {
         </ErrorBoundary>
       </Box>
       <WizardPanel.Actions>
-        <WizardPanel.ActionNext disabled={!dirty || !isValid}>Continue</WizardPanel.ActionNext>
+        {shouldSkipCFNUpload ? (
+          <WizardPanel.ActionGoToStep disabled={!dirty || !isValid} stepIndex={2} />
+        ) : (
+          <WizardPanel.ActionNext disabled={!dirty || !isValid}>Continue</WizardPanel.ActionNext>
+        )}
       </WizardPanel.Actions>
     </WizardPanel>
   );
--- web/src/components/wizards/S3LogSourceWizard/StackDeploymentPanel/StackDeployment.tsx
+++ web/src/components/wizards/S3LogSourceWizard/StackDeploymentPanel/StackDeployment.tsx
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { Text, Box, Flex, Card, Img, Heading, Button, SimpleGrid, useSnackbar } from 'pouncejs';
+import { Text, Box, Flex, Card, Img, Heading, Button, useSnackbar } from 'pouncejs';
 import React from 'react';
 import { downloadData, toStackNameFormat } from 'Helpers/utils';
 import { useFormikContext } from 'formik';
@@ -62,7 +62,7 @@ const StackDeployment: React.FC = () => {
         title="Deploy Panther's IAM roles"
         subtitle="These roles will allow Panther to read your logs from the S3 Bucket"
       />
-      <SimpleGrid columns={2} gap={5} px={80} mx="auto" mb={6}>
+      <Flex spacing={6} px={80} mx="auto" mb={6} justify="center">
         {!initialValues.integrationId && (
           <Card variant="dark" p={6}>
             <Flex direction="column" align="center" spacing={4}>
@@ -86,7 +86,7 @@ const StackDeployment: React.FC = () => {
             </Flex>
           </Card>
         )}
-        <Card variant="dark" p={6}>
+        <Card variant="dark" p={6} maxWidth="50%">
           <Flex direction="column" align="center" spacing={4}>
             <Img src={cogsIllustration} alt="Cogssn" nativeWidth={40} nativeHeight={40} />
             <Heading as="h4" size="x-small">
@@ -116,7 +116,7 @@ const StackDeployment: React.FC = () => {
             </Button>
           </Flex>
         </Card>
-      </SimpleGrid>
+      </Flex>
       <WizardPanel.Actions>
         <WizardPanel.ActionPrev />
         <Flex spacing={4} direction="column" align="center">
