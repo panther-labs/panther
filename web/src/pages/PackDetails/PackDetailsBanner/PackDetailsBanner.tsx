d11080b66b35233c26b6b00f7870e45e49de47c7
Alex Mylonas <alex.a.mylonas@gmail.com>
2021-02-16 14:44:20 +0200

    chore: added callback for functions

--- web/src/pages/PackDetails/PackDetailsBanner/PackDetailsBanner.tsx
+++ web/src/pages/PackDetails/PackDetailsBanner/PackDetailsBanner.tsx
@@ -78,18 +78,21 @@ const PackDetailsBanner: React.FC<ResourceDetailsInfoProps> = ({ pack }) => {
     },
   });
 
-  const onPatch = (values: UpdateVersionFormValues) => {
-    return updatePack({
-      variables: {
-        input: {
-          id: pack.id,
-          versionId: values.packVersion.id,
+  const onPatch = React.useCallback(
+    (values: UpdateVersionFormValues) => {
+      return updatePack({
+        variables: {
+          input: {
+            id: pack.id,
+            versionId: values.packVersion.id,
+          },
         },
-      },
-    });
-  };
+      });
+    },
+    [pack]
+  );
 
-  const onStatusUpdate = () => {
+  const onStatusUpdate = React.useCallback(() => {
     return updatePack({
       variables: {
         input: {
@@ -98,76 +101,74 @@ const PackDetailsBanner: React.FC<ResourceDetailsInfoProps> = ({ pack }) => {
         },
       },
     });
-  };
+  }, [pack]);
 
   return (
-    <React.Fragment>
-      <Card as="article" position="relative">
-        {loading && (
-          <Flex
-            position="absolute"
-            direction="column"
-            spacing={2}
-            backgroundColor="navyblue-700"
-            height="100%"
-            zIndex={2}
-            alignItems="center"
-            opacity={0.9}
-            justify="center"
-            width={1}
-          >
-            <Text textAlign="center" opacity={1} fontWeight="bold">
-              {pack.displayName || pack.id}
-            </Text>
-            <Text textAlign="center" opacity={1}>
-              is being updated, please wait.
-            </Text>
-            <BulletedLoading />
-          </Flex>
-        )}
-        <Flex p={6}>
-          <Box>
-            <Flex as="header" align="center">
-              <Heading
-                fontWeight="bold"
-                wordBreak="break-word"
-                aria-describedby="rule-description"
-                flexShrink={1}
-                display="flex"
-                alignItems="center"
-                mr={4}
-              >
-                {pack.displayName || pack.id}
-              </Heading>
-              {pack.updateAvailable && (
-                <Box
-                  as="span"
-                  backgroundColor={pack.enabled ? 'red-500' : 'gray-500'}
-                  borderRadius="small"
-                  px={2}
-                  py={1}
-                  fontSize="small"
-                  fontWeight="bold"
-                >
-                  UPDATE AVAILABLE
-                </Box>
-              )}
-            </Flex>
-            <Flex as="dl" fontSize="medium" pt={2} spacing={8} wrap="wrap">
-              {pack.description}
-            </Flex>
-          </Box>
-          <Flex align="center" spacing={8} flexShrink={0} ml="auto">
-            <Flex ml="auto" mr={0} align="flex-end">
-              <Switch onClick={onStatusUpdate} label="Enabled" checked={pack.enabled} />
-            </Flex>
-            <Box width="250px">
-              <UpdateVersion pack={pack} onPatch={onPatch} />
-            </Box>
-          </Flex>
+    <Card as="article" position="relative">
+      {loading && (
+        <Flex
+          position="absolute"
+          direction="column"
+          spacing={2}
+          backgroundColor="navyblue-700"
+          height="100%"
+          zIndex={2}
+          alignItems="center"
+          opacity={0.9}
+          justify="center"
+          width={1}
+        >
+          <Text textAlign="center" opacity={1} fontWeight="bold">
+            {pack.displayName || pack.id}
+          </Text>
+          <Text textAlign="center" opacity={1}>
+            is being updated, please wait.
+          </Text>
+          <BulletedLoading />
         </Flex>
-      </Card>
-    </React.Fragment>
+      )}
+      <Flex p={6}>
+        <Box>
+          <Flex as="header" align="center">
+            <Heading
+              fontWeight="bold"
+              wordBreak="break-word"
+              aria-describedby="rule-description"
+              flexShrink={1}
+              display="flex"
+              alignItems="center"
+              mr={4}
+            >
+              {pack.displayName || pack.id}
+            </Heading>
+            {pack.updateAvailable && (
+              <Box
+                as="span"
+                backgroundColor={pack.enabled ? 'red-500' : 'gray-500'}
+                borderRadius="small"
+                px={2}
+                py={1}
+                fontSize="small"
+                fontWeight="bold"
+              >
+                UPDATE AVAILABLE
+              </Box>
+            )}
+          </Flex>
+          <Flex as="dl" fontSize="medium" pt={2} spacing={8} wrap="wrap">
+            {pack.description}
+          </Flex>
+        </Box>
+        <Flex align="center" spacing={8} flexShrink={0} ml="auto">
+          <Flex ml="auto" mr={0} align="flex-end">
+            <Switch onClick={onStatusUpdate} label="Enabled" checked={pack.enabled} />
+          </Flex>
+          <Box width="250px">
+            <UpdateVersion pack={pack} onPatch={onPatch} />
+          </Box>
+        </Flex>
+      </Flex>
+    </Card>
   );
 };
 
