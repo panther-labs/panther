c6921e71f27e323f0833c9ba142f2cf012b56604
Nicholas Hakmiller <nicholas.hakmiller@runpanther.io>
2021-02-19 17:02:27 -0800

    ignore some verbose logs

--- internal/compliance/aws_event_processor/processor/process.go
+++ internal/compliance/aws_event_processor/processor/process.go
@@ -252,12 +252,25 @@ var (
 		"CreateClusterParameterGroup":       {},
 
 		// s3
-		"UploadPart":              {},
-		"CreateMultipartUpload":   {},
+		"AbortMultipartUpload": {},
 		"CompleteMultipartUpload": {},
+		"CopyObject": {},
+		"CreateMultipartUpload":   {},
+		"DeleteObject": {},
+		"DeleteObjects": {},
+		"DeleteObjectTagging": {},
 		"HeadBucket":              {},
-		"HeadObject":              {},
-		"PutObject":               {},
+		"HeadObject": {},
+		"PutObject": {},
+		"PutObjectAcl": {},
+		"PutObjectLegalHold": {},
+		"PutObjectLockConfiguration": {},
+		"PutObjectRetention": {},
+		"PutObjectTagging": {},
+		"RestoreObject": {},
+		"SelectObjectContent": {},
+		"UploadPart":              {},
+		"UploadPartCopy": {},
 
 		// waf, waf-regional
 		// TODO get suffixes
